186461c10c5b69f8881bbf6d73945c864cd335b7
Christiaan Seerden <christiaanseerden@live.nl>
2024-08-08 11:38:38 +0200

    set up testing configs

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,7 +1,10 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-export default {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
+module.exports = {
+	preset: "ts-jest/presets/js-with-ts",
+	testEnvironment: "jsdom",
+	moduleDirectories: ["node_modules", "src"],
+	setupFilesAfterEnv: ["./setupJest.js"],
+	transform: {
+		"node_modules/variables/.+\\.(j|t)sx?$": "ts-jest",
+	},
+	transformIgnorePatterns: ["node_modules/(?!variables/.*)"],
 };
