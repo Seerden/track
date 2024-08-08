2f3398a5b2ea43a6cf2304e9d6d91aa05ac1324e
Christiaan Seerden <christiaanseerden@live.nl>
2024-08-08 11:57:03 +0200

    fix linting setup

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -8,7 +8,6 @@ module.exports = {
 		"eslint:recommended",
 		"plugin:react/recommended",
 		"plugin:@typescript-eslint/recommended",
-		"plugin:storybook/recommended",
 		"prettier",
 	],
 	parser: "@typescript-eslint/parser",
