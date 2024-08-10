2b704970f470b1ee70f6ddd7aaa49a7acaebd19d
Christiaan Seerden <christiaanseerden@live.nl>
2024-08-10 13:39:14 +0200

    use dayjs relativeTime and duration plugins

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,14 @@
+import dayjs from "dayjs";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
 import { worker } from "./mocks/browser.ts";
+
+import duration from "dayjs/plugin/duration";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
 
 if (process.env.NODE_ENV === "development") {
 	worker.start();
