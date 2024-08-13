488b89ebac443eadf25ac18d4a044a56c9eec008
Christiaan Seerden <christiaanseerden@live.nl>
2024-08-13 11:48:25 +0200

    split up tag types

--- server/types/data/tag.types.ts
+++ server/types/data/tag.types.ts
@@ -1,11 +1,17 @@
 import { ID, Timestamp } from "./utility.types";
 
-export type Tag = {
+export type NewTag = {
 	user_id: ID;
-	tag_id: ID;
-	category_id?: Tag["tag_id"];
-	subcategory_id?: Tag["tag_id"];
 	name: string;
 	description?: string;
+};
+
+export type Tag = NewTag & {
 	created_at: Timestamp;
+};
+
+export type TagWithIds = Tag & {
+	tag_id: ID;
+	category_id?: TagWithIds["tag_id"];
+	subcategory_id?: TagWithIds["tag_id"];
 };
