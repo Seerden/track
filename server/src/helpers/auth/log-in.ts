17d29b672fc88f030a93954dea98b9a4d69ad2ae
Christiaan Seerden <christiaanseerden@live.nl>
2024-08-14 15:20:43 +0200

    log in request handler

---
+++ server/src/helpers/auth/log-in.ts
@@ -0,0 +1,23 @@
+import { compare } from "bcryptjs";
+import { Request, Response } from "express";
+import { UserLogin } from "../../../types/data/user.types";
+import { getUserByName } from "../data/query-user";
+import { destroySession } from "./destroy-session";
+
+/** Request handler. If login info is value, (re-)set the session. */
+export async function login(user: UserLogin, req: Request, res: Response) {
+	const foundUser = await getUserByName({ username: user.username });
+
+	if (!foundUser) {
+		return res.status(404).json({ message: "No user found for given credentials." });
+	}
+
+	const passwordMatches = await compare(user.password, foundUser.password_hash);
+
+	if (!passwordMatches) {
+		return destroySession({ req, res });
+	}
+
+	req.session.user_id = foundUser.user_id; // could also regenerate()
+	return res.status(200).json({ user: foundUser });
+}
