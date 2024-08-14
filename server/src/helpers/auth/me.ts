51d194b558338732a0d8b733dbadc25b4d43bdad
Christiaan Seerden <christiaanseerden@live.nl>
2024-08-14 15:20:20 +0200

    query currently logged in user

---
+++ server/src/helpers/auth/me.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { getUserById } from "../data/query-user";
+
+/** Request handler that returns the active user, or an error message. */
+export async function getMe({ req, res }: { req: Request; res: Response }) {
+	if (req.session?.user_id) {
+		return res
+			.status(200)
+			.json({ user: await getUserById({ user_id: req.session.user_id }) });
+	}
+
+	return res.status(401).json({ user: null, message: "No active user." });
+}
