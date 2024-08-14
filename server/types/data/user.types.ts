5a1bc1ff85ededeebe54713ba69cca53718d732c
Christiaan Seerden <christiaanseerden@live.nl>
2024-08-14 13:32:13 +0200

    split up user types

--- server/types/data/user.types.ts
+++ server/types/data/user.types.ts
@@ -1,9 +1,16 @@
 import { Timestamp } from "./utility.types";
 
-export type User = {
-	user_id: number;
+export type NewUser = {
 	username: string;
+	password: string;
 	email?: string;
+};
+
+export type UserInput = Omit<NewUser, "password"> & {
 	password_hash: string;
+};
+
+export type User = UserInput & {
+	user_id: number;
 	created_at: Timestamp;
 };
