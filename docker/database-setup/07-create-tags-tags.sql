e33372d4b53bae89a2c5203a9abba8e6594a745f
Christiaan Seerden <christiaanseerden@live.nl>
2024-08-23 11:33:10 +0200

    add user_id field to tags_tags table

--- docker/database-setup/07-create-tags-tags.sql
+++ docker/database-setup/07-create-tags-tags.sql
@@ -1,8 +1,9 @@
 create table if not exists tags_tags (
-  parent_id serial not null,
-  child_id serial not null,
-  primary key (parent_id, child_id),
-  foreign key (parent_id) references tags (tag_id),
-  foreign key (child_id) references tags (tag_id),
-  constraint ids_not_equal check (parent_id <> child_id)
+   user_id serial not null,
+   parent_id serial not null,
+   child_id serial not null,
+   primary key (parent_id, child_id),
+   foreign key (parent_id) references tags (tag_id),
+   foreign key (child_id) references tags (tag_id),
+   constraint ids_not_equal check (parent_id <> child_id)
 );
