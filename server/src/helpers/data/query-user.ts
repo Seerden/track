a596ade8e17158888b78d55534061441ee6ebdf4
Christiaan Seerden <christiaanseerden@live.nl>
2024-08-23 11:34:34 +0200

    types:remove usage of implicit any

--- server/src/helpers/data/query-user.ts
+++ server/src/helpers/data/query-user.ts
@@ -1,17 +1,28 @@
-import { User } from "../../../types/data/user.types";
+import { type User } from "../../../types/data/user.types";
+import { type ID } from "../../../types/data/utility.types";
+import { type WithSQL } from "../../../types/sql.types";
 import { sqlConnection } from "../../db/init";
 
-export async function userExists({ sql = sqlConnection, username }) {
+export async function userExists({
+	sql = sqlConnection,
+	username,
+}: WithSQL<{ username: string }>) {
 	const [user] = await sql<[User]>`select * from users where username = ${username}`;
 	return user;
 }
 
-export async function getUserByName({ sql = sqlConnection, username }) {
+export async function getUserByName({
+	sql = sqlConnection,
+	username,
+}: WithSQL<{ username: string }>) {
 	const [user] = await sql<[User]>`select * from users where username = ${username}`;
 	return user;
 }
 
-export async function getUserById({ sql = sqlConnection, user_id }) {
+export async function getUserById({
+	sql = sqlConnection,
+	user_id,
+}: WithSQL<{ user_id: ID }>) {
 	const [user] = await sql<[User]>`select * from users where user_id = ${user_id}`;
 	return user;
 }
