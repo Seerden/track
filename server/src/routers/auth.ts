74d97322adae2852ea427d46e9671e3661ba77b9
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-02 15:04:00 +0200

    doc:add TODO for registration endpoint

--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -6,6 +6,8 @@ import { getUserById } from "../helpers/data/query-user";
 
 export const authRouter = Router({ mergeParams: true });
 
+// TODO add registration POST route
+
 authRouter.get("/me", async (req, res) => {
 	if (!req.session?.user_id) {
 		return res.status(401).json({ user: null, message: "No active user." });
