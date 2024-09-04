ea6f929878c7cffe50a0a684feeb4a4a21b507fc
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-04 19:24:08 +0200

    implement an endpoint that fetches all of a user's tags as tagsById

--- server/src/routers/data.ts
+++ server/src/routers/data.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { NewActivity } from "../../types/data/activity.types";
+import { isAuthorized } from "../helpers/auth/is-authorized";
 import { insertActivity } from "../helpers/data/insert-activity";
+import { getTagsWithRelations } from "../helpers/data/merge-tags-and-relations";
 
 export const dataRouter = Router({ mergeParams: true });
 
@@ -11,4 +13,12 @@ dataRouter.post("/activity", async (req, res) => {
 	const activity = await insertActivity({ newActivity });
 
 	res.json({ activity });
+});
+
+dataRouter.get("/tags", isAuthorized, async (req, res) => {
+	const user_id = req.session.user?.user_id!; // always exists if we're here, because of middleware
+
+	const tagsById = await getTagsWithRelations({ user_id });
+
+	res.json({ tagsById });
 });
