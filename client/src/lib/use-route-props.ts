abb0f335400a595298ec2590734647a1d71cc9cd
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-04 19:37:27 +0200

    implement a route props hook

---
+++ client/src/lib/use-route-props.ts
@@ -0,0 +1,11 @@
+import { useLocation, useNavigate, useParams } from "react-router";
+
+function useRouteProps() {
+	return {
+		location: useLocation(),
+		navigate: useNavigate(),
+		params: useParams(),
+	};
+}
+
+export default useRouteProps;
