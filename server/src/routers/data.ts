f85105ffe78394548ff93258efe7b5ded2fa0fe2
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-06 14:17:42 +0200

    implement POST endpoint for new tag creation

--- server/src/routers/data.ts
+++ server/src/routers/data.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
-import { NewActivity } from "../../types/data/activity.types";
+import type { NewActivity } from "../../types/data/activity.types";
+import type { NewTag } from "../../types/data/tag.types";
+import type { ID } from "../../types/data/utility.types";
 import { isAuthorized } from "../helpers/auth/is-authorized";
 import { insertActivity } from "../helpers/data/insert-activity";
+import { insertTagWithRelation } from "../helpers/data/insert-tags";
 import { getTagsWithRelations } from "../helpers/data/merge-tags-and-relations";
 
 export const dataRouter = Router({ mergeParams: true });
@@ -21,4 +24,12 @@ dataRouter.get("/tags", isAuthorized, async (req, res) => {
 	const tagsById = await getTagsWithRelations({ user_id });
 
 	res.json({ tagsById });
+});
+
+dataRouter.post("/tag", isAuthorized, async (req, res) => {
+	const { newTag, parent_id } = req.body as { newTag: NewTag; parent_id?: ID };
+
+	const tag = await insertTagWithRelation({ newTag, parent_id });
+
+	res.json({ tag });
 });
