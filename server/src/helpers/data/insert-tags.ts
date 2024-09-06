8872287e8b7729ddf3a9fe13a8d61d84384f6b33
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-06 14:17:19 +0200

    implement a function that creates a single tag with a parent, if specified

--- server/src/helpers/data/insert-tags.ts
+++ server/src/helpers/data/insert-tags.ts
@@ -1,5 +1,5 @@
 import { TagTagRelation } from "../../../types/data/relational.types";
-import { NewTag, Tag } from "../../../types/data/tag.types";
+import { NewTag, TagWithId, TagWithIds } from "../../../types/data/tag.types";
 import { ID } from "../../../types/data/utility.types";
 import type { WithSQL } from "../../../types/sql.types";
 import { sqlConnection } from "../../db/init";
@@ -9,7 +9,7 @@ export async function insertTags({
 	sql = sqlConnection,
 	newTags,
 }: WithSQL<{ newTags: Array<NewTag> }>) {
-	const insertedTags = sql<[Tag?]>`
+	const insertedTags = sql<[TagWithId]>`
         insert into tags 
         ${sql(newTags)}
         returning *
@@ -24,10 +24,31 @@ export async function insertTagRelation({
 	parent_id,
 	child_id,
 }: WithSQL<{ user_id: ID; parent_id: ID; child_id: ID }>) {
-	const [relation] = await sql<[TagTagRelation?]>`
+	const [relation] = await sql<[TagTagRelation]>`
         insert into tag_relations
         ${sql({ user_id, parent_id, child_id })}
         returning *
     `;
 	return relation;
+}
+
+export async function insertTagWithRelation({
+	sql = sqlConnection,
+	newTag,
+	parent_id,
+}: WithSQL<{ newTag: NewTag; parent_id?: ID }>): Promise<TagWithIds> {
+	const [tag] = await insertTags({ sql, newTags: [newTag] });
+
+	if (!parent_id) {
+		return Object.assign(tag, { child_ids: [], parent_id: null });
+	}
+
+	const relation = await insertTagRelation({
+		sql,
+		user_id: tag.user_id,
+		parent_id,
+		child_id: tag.tag_id,
+	});
+
+	return Object.assign(tag, { child_ids: [], parent_id: relation.parent_id });
 }
