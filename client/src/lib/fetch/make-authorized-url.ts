741f4fa746a1520653e24f8e1278f2ac5cd5baeb
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-09 14:50:53 +0200

    take duplicate ? out of authorizedUrl

--- client/src/lib/fetch/make-authorized-url.ts
+++ client/src/lib/fetch/make-authorized-url.ts
@@ -5,5 +5,5 @@ export function makeAuthorizedUrl(url: string) {
 	const user_id = localUser.get()?.user_id;
 	const params = user_id ? `?${new URLSearchParams({ user_id: String(user_id) })}` : "";
 
-	return `${baseUrl}${url}?${params}`;
+	return `${baseUrl}${url}${params}`;
 }
