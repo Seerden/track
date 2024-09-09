79d0151ff7ab78ca87c61e26aae680c53f1fc906
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-09 12:49:22 +0200

    fix makeAuthorizedUrl logic to prevent undefined values getting in

--- client/src/lib/fetch/make-authorized-url.ts
+++ client/src/lib/fetch/make-authorized-url.ts
@@ -2,7 +2,8 @@ import { localUser } from "../user-storage";
 import { baseUrl } from "./fetch-constants";
 
 export function makeAuthorizedUrl(url: string) {
-	const params = new URLSearchParams({ user_id: `${localUser.get()?.user_id}` });
+	const user_id = localUser.get()?.user_id;
+	const params = user_id ? `?${new URLSearchParams({ user_id: String(user_id) })}` : "";
 
-	return `${baseUrl}${url}${params}`;
+	return `${baseUrl}${url}?${params}`;
 }
