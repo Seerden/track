88162da368f6968e152db2f1bd48ac7bbf59ba34
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-09 12:50:38 +0200

    save session server-side on login, just in case

--- server/src/helpers/auth/log-in.ts
+++ server/src/helpers/auth/log-in.ts
@@ -19,5 +19,7 @@ export async function login(user: UserLogin, req: Request, res: Response) {
 	}
 
 	req.session.user = { user_id: foundUser.user_id, username: foundUser.username }; // could also regenerate()
+	req.session.save();
+
 	return res.status(200).json({ user: foundUser });
 }
