5727c4bc7abcd565137b33bba88545e2d1ff5f73
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-10 01:54:15 +0200

    implement client-side query for the new tag tree GET endpoint

---
+++ client/src/lib/fetch/use-tags-tree-query.ts
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import type { Data } from "../../types/query.types";
+import type { ID } from "../../types/server/utility.types";
+import { defaultQueryConfig } from "../query-client";
+import { makeAuthorizedUrl } from "./make-authorized-url";
+
+export async function getTagsTree() {
+	const url = makeAuthorizedUrl("/data/tags/tree");
+	const response = await fetch(url, {
+		credentials: "include",
+		method: "GET",
+	});
+	return response.json();
+}
+
+type TagsTreeData = Data<"tree", Record<ID, { members: number[] }>>;
+
+export default function useTagsTreeQuery() {
+	return useQuery<TagsTreeData>({
+		// TODO: if someone switches accounts, they might get the wrong tags --
+		// make sure to invalidate all user-data queries when the user changes, or
+		// add user_id to the queryKey
+		queryKey: ["tags", "tree"],
+		queryFn: getTagsTree,
+		...defaultQueryConfig,
+	});
+}
