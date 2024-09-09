768cf3d832b5251c9a95031e5e8835ae38601014
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-09 14:50:21 +0200

    create hook for NewTag functionality

---
+++ client/src/components/NewTag/use-new-tag.ts
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useSelectedTags } from "../../lib/state/selected-tags-state";
+import useAuthentication from "../../lib/use-authentication";
+import type { NewTag } from "../../types/server/tag.types";
+import { useNewTagMutation } from "./use-new-tag-mutation";
+
+export default function useNewTag() {
+	const { currentUser } = useAuthentication();
+
+	const { mutate: submit } = useNewTagMutation();
+
+	const [newTag, setNewTag] = useState<NewTag>({
+		name: "",
+		user_id: currentUser!.user_id, // TODO: find a way to refine this typing so that currentUser cannot be undefined here
+	});
+
+	const { selectedTagIds } = useSelectedTags();
+	const parent_id = selectedTagIds.length === 1 ? selectedTagIds[0] : undefined;
+
+	function onInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+		setNewTag((current) => ({ ...current, [e.target.name]: e.target.value }));
+	}
+
+	function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+
+		submit({ newTag, parent_id });
+	}
+
+	return {
+		onInputChange,
+		onSubmit,
+	};
+}
