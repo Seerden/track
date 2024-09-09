61198fe94b9259bb4a4aa46222ed30816a66ab8e
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-09 11:31:52 +0200

    render Login component inside Protected in case user is not logged in yet

--- client/src/components/Protected.tsx
+++ client/src/components/Protected.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from "react";
 import useAuthentication from "../lib/use-authentication";
 import useRouteProps from "../lib/use-route-props";
+import Login from "./Login/Login";
 
 export default function Protected({ children }: PropsWithChildren) {
 	const { isLoggedIn, currentUser } = useAuthentication();
@@ -25,7 +26,12 @@ export default function Protected({ children }: PropsWithChildren) {
 		// this is temporary, maybe redirect to a Login. also, maybe give that an
 		// optional property redirectTo which we can then route to on successful
 		// login
-		return <div>Must be logged in to view this component.</div>;
+		return (
+			<main>
+				You must be logged in to view this component.
+				<Login />
+			</main>
+		);
 	}
 
 	return <>{children}</>;
