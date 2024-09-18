35c89bdab75c3712392231842db8d416527c746d
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-18 19:40:01 +0200

    wip:render NewNote in Home component for dev purposes

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,7 +1,13 @@
+import NewNote from "./NewNote/NewNote";
 import NewTag from "./NewTag/NewTag";
 
 function Home() {
-	return <NewTag />;
+	return (
+		<>
+			<NewTag />
+			<NewNote />
+		</>
+	);
 }
 
 export default Home;
