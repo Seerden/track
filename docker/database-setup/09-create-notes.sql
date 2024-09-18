504caa6a4cddb3b45e45ba6cf0ad3177958d0fc9
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-18 19:49:12 +0200

    create notes table

---
+++ docker/database-setup/09-create-notes.sql
@@ -0,0 +1,9 @@
+create table if not exists notes (
+    note_id serial unique not null primary key,
+    user_id serial not null references users(user_id),
+    activity_id serial references activities(activity_id),
+    title varchar(255) default null,
+    content text not null,
+    created_at timestamp default now(),
+    date date
+);
