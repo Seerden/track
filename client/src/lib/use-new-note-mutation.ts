62a33291d4cfd5c7f1b76115bb641d29104dbda0
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-19 00:35:24 +0200

    implement query and mutation hook for note(s)

---
+++ client/src/lib/use-new-note-mutation.ts
@@ -0,0 +1,18 @@
+import { useMutation } from "@tanstack/react-query";
+import type { NoteInput, NoteWithIds } from "../types/server/note.types";
+import { createPostConfig } from "./fetch/create-post-config";
+import { makeAuthorizedUrl } from "./fetch/make-authorized-url";
+
+async function postNote({ note, tagIds }: NoteInput) {
+	const url = makeAuthorizedUrl("/data/note");
+	return (await fetch(url, createPostConfig({ note, tagIds }))).json();
+}
+
+export function useNewNoteMutation() {
+	return useMutation<NoteWithIds, unknown, NoteInput>({
+		async mutationFn({ note, tagIds }) {
+			return postNote({ note, tagIds });
+		},
+		mutationKey: ["new-note"],
+	});
+}
