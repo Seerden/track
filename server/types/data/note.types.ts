8d667785fe4d06a636e922e6e3285f5c862671cc
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-18 20:00:31 +0200

    types:implement NewNote type

--- server/types/data/note.types.ts
+++ server/types/data/note.types.ts
@@ -1,11 +1,14 @@
 import type { ID, Maybe, Timestamp } from "./utility.types";
 
-export type Note = {
-	note_id: ID;
+export type NewNote = {
 	user_id: ID;
 	activity_id: Maybe<ID>;
 	title?: string;
 	content: string;
-	created_at: Timestamp;
 	date: Timestamp; // TODO: this is a date in postgres -- how does it get parsed?
+};
+
+export type Note = NewNote & {
+	note_id: ID;
+	created_at: Timestamp;
 };
