c48f3b332535a438c6410f8bf3e0668d49e66661
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-23 22:34:30 +0200

    default initialOpen to true, add a todo for future

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -2,11 +2,14 @@ import { PropsWithChildren, useRef } from "react";
 import useModal from "../lib/use-modal";
 import * as S from "./Modal.style";
 
-export default function Modal({
-	children,
-	initialOpen
-}: PropsWithChildren<{ initialOpen: boolean }>) {
+export default function Modal(
+	{ children, initialOpen }: PropsWithChildren<{ initialOpen: boolean }> = {
+		initialOpen: true
+	}
+) {
 	const modalRef = useRef(null);
+	// TODO: if initialOpen is false, we need a way to open the modal. Wait for a
+	// use case before thinking about what to do in that case.
 	const { isOpen, close } = useModal(modalRef, { initialOpen });
 
 	if (!isOpen) {
