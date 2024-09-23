b74fd5fcc47355052941af2c38af3d40c6215e0b
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-23 02:09:45 +0200

    remove a console.log call

--- client/src/components/Notes/use-notes.ts
+++ client/src/components/Notes/use-notes.ts
@@ -1,12 +1,7 @@
-import { useEffect } from "react";
 import useNotesQuery from "../../lib/use-notes-query";
 
 export default function useNotes() {
 	const { data } = useNotesQuery();
-
-	useEffect(() => {
-		console.log({ data });
-	}, [data]);
 
 	return {
 		data,
