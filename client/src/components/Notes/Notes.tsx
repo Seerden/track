d29e086cce672fa37ccea51bd0d384fc9797e8d8
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-23 13:40:37 +0200

    extract NoteElement to small subcomponent

--- client/src/components/Notes/Notes.tsx
+++ client/src/components/Notes/Notes.tsx
@@ -1,6 +1,30 @@
 import { formatDate } from "../../lib/format-date";
+import type { TagsData } from "../../types/data.types";
+import type { NoteWithIds } from "../../types/server/note.types";
 import * as S from "./Notes.style";
 import useNotes from "./use-notes";
+
+function makeNoteTitle(note: NoteWithIds) {
+	return note.title?.length ? note.title : formatDate(note.created_at);
+}
+
+function NoteElement({ note, tags }: { note: NoteWithIds; tags?: TagsData }) {
+	const title = makeNoteTitle(note);
+
+	return (
+		<S.Note key={note.note_id}>
+			<S.NoteHeader>
+				<S.Title>{title}</S.Title>
+				<S.Tags>
+					{note.tag_ids?.map((id) => (
+						<S.Tag key={id}>{tags?.tagsById?.[id]?.name}</S.Tag>
+					))}
+				</S.Tags>
+			</S.NoteHeader>
+			<p>{note.content}</p>
+		</S.Note>
+	);
+}
 
 export default function Notes() {
 	const { notes, tags } = useNotes();
@@ -14,25 +38,9 @@ export default function Notes() {
 			<h1>My notes 📔</h1>
 
 			<S.List>
-				{Object.values(notes.notesById).map((note) => {
-					const title = note.title?.length
-						? note.title
-						: formatDate(note.created_at);
-
-					return (
-						<S.Note key={note.note_id}>
-							<S.NoteHeader>
-								<S.Title>{title}</S.Title>
-								<S.Tags>
-									{note.tag_ids?.map((id) => (
-										<S.Tag key={id}>{tags?.tagsById?.[id]?.name}</S.Tag>
-									))}
-								</S.Tags>
-							</S.NoteHeader>
-							<p>{note.content}</p>
-						</S.Note>
-					);
-				})}
+				{Object.values(notes.notesById).map((note) => (
+					<NoteElement key={note.note_id} note={note} tags={tags} />
+				))}
 			</S.List>
 		</S.Page>
 	);
