1d910f736712f49193fbc5e3c8845515302f63b8
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-25 19:53:32 +0200

    database queries for activities and a GET endpoint

---
+++ server/src/helpers/data/merge-activities-and-relations.ts
@@ -0,0 +1,19 @@
+import type { Activity, ActivityWithIds } from "../../../types/data/activity.types";
+import type { ActivityTagRelation } from "../../../types/data/relational.types";
+import type { ById } from "../../../types/data/utility.types";
+
+export async function mergeActivitiesAndRelations(
+	activities: Activity[],
+	activityTagRelations: ActivityTagRelation[]
+) {
+	const activitiesById = {} as ById<ActivityWithIds>;
+	for (const activity of activities) {
+		activitiesById[activity.activity_id] = Object.assign(activity, { tag_ids: [] });
+	}
+
+	for (const { activity_id, tag_id } of activityTagRelations) {
+		activitiesById[activity_id]?.tag_ids?.push(tag_id);
+	}
+
+	return activitiesById;
+}
---
+++ server/src/helpers/data/query-activities.ts
@@ -0,0 +1,32 @@
+import { Activity } from "../../../types/data/activity.types";
+import { ActivityTagRelation } from "../../../types/data/relational.types";
+import { ID } from "../../../types/data/utility.types";
+import { WithSQL } from "../../../types/sql.types";
+import { sqlConnection } from "../../db/init";
+import { mergeActivitiesAndRelations } from "./merge-activities-and-relations";
+
+export async function queryActivitiesByUser({
+	sql = sqlConnection,
+	user_id,
+}: WithSQL<{ user_id: ID }>) {
+	return sql<Activity[]>`select * from activities where user_id = ${user_id}`;
+}
+
+export async function queryActivityTags({
+	sql = sqlConnection,
+	user_id,
+}: WithSQL<{ user_id: ID }>) {
+	return sql<
+		ActivityTagRelation[]
+	>`select * from activities_tags where user_id = ${user_id}`;
+}
+
+export async function queryActivitiesAndRelations({
+	sql = sqlConnection,
+	user_id,
+}: WithSQL<{ user_id: ID }>) {
+	const activities = await queryActivitiesByUser({ sql, user_id });
+	const activityTagRelations = await queryActivityTags({ sql, user_id });
+
+	return mergeActivitiesAndRelations(activities, activityTagRelations);
+}
