3cac7b5e490c56c9c1f01f0b28b48269f0c55e13
Christiaan Seerden <christiaanseerden@live.nl>
2024-09-26 12:07:20 +0200

    give formatDate an optional short argument that does not show seconds

--- client/src/lib/format-date.ts
+++ client/src/lib/format-date.ts
@@ -3,7 +3,7 @@ import dayjs, { Dayjs } from "dayjs";
 type Datelike = Date | string | number | Dayjs;
 
 // format a dayjs date to year-month-date hh:mm:ss
-export function formatDate(date: Datelike): string {
+export function formatDate(date: Datelike, { short }: { short?: boolean } = {}): string {
 	const d = dayjs(date);
-	return d.format("YYYY-MM-DD HH:mm:ss");
+	return d.format(`YYYY-MM-DD HH:mm${!short ? ":ss" : ""}`);
 }
