c28caf748ed6442ce6ce83244989f53508bac928
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-02 14:23:44 +0200

    use utc before formatting date

--- client/src/lib/format-date.ts
+++ client/src/lib/format-date.ts
@@ -2,8 +2,8 @@ import dayjs, { Dayjs } from "dayjs";
 
 type Datelike = Date | string | number | Dayjs;
 
-// format a dayjs date to year-month-date hh:mm:ss
+// Format a date to year-month-date hh:mm (and optionally :ss)
 export function formatDate(date: Datelike, { short }: { short?: boolean } = {}): string {
-	const d = dayjs(date);
+	const d = dayjs.utc(date); // TODO: honestly I'm not sure why we need to use utc here, but it gets parsed incorrectly if we don't
 	return d.format(`YYYY-MM-DD HH:mm${!short ? ":ss" : ""}`);
 }
