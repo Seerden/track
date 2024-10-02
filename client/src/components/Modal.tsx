453b823728a273344460a485e3614341fc1f86a9
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-03 00:08:04 +0200

    implement an outside state handler for modals

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -3,15 +3,20 @@ import { createPortal } from "react-dom";
 import useModal from "../lib/use-modal";
 import * as S from "./Modal.style";
 
+type ModalProps = {
+	initialOpen: boolean;
+	outsideStateHandler?: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
 export default function Modal(
-	{ children, initialOpen }: PropsWithChildren<{ initialOpen: boolean }> = {
+	{ children, initialOpen, outsideStateHandler }: PropsWithChildren<ModalProps> = {
 		initialOpen: true
 	}
 ) {
 	const modalRef = useRef(null);
 	// TODO: if initialOpen is false, we need a way to open the modal. Wait for a
 	// use case before thinking about what to do in that case.
-	const { isOpen, close } = useModal(modalRef, { initialOpen });
+	const { isOpen, close } = useModal(modalRef, { initialOpen, outsideStateHandler });
 
 	if (!isOpen) {
 		return null;
