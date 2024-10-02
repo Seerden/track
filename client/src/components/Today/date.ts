7078c1ea3d3da1f46edea50c414c55c305ec0881
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-03 00:57:19 +0200

    use local datetime in sameDay function

--- client/src/components/Today/date.ts
+++ client/src/components/Today/date.ts
@@ -2,5 +2,5 @@ import dayjs from "dayjs";
 import { Datelike } from "../../types/date.types";
 
 export function sameDay(one: Datelike, two: Datelike) {
-	return dayjs(one).isSame(dayjs(two), "day");
+	return dayjs.utc(one).local().isSame(dayjs.utc(two).local(), "day");
 }
