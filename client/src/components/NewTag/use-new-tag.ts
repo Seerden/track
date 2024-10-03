36bdabeb886262951a16bb91bdc34a3bfdcab832
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-03 12:43:50 +0200

    reset tag selection on useNewTag mount; todo: on separating tag selection state per use-case

--- client/src/components/NewTag/use-new-tag.ts
+++ client/src/components/NewTag/use-new-tag.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTagSelection } from "../../lib/state/selected-tags-state";
 import useAuthentication from "../../lib/use-authentication";
 import useTagsQuery from "../../lib/use-tags-query";
@@ -15,8 +15,16 @@ export default function useNewTag() {
 		user_id: currentUser!.user_id, // TODO: find a way to refine this typing so that currentUser cannot be undefined here
 	});
 
-	const { selectedTagIds } = useTagSelection();
+	const { selectedTagIds, resetTagSelection } = useTagSelection();
 	const parent_id = selectedTagIds.length === 1 ? selectedTagIds[0] : undefined;
+
+	useEffect(() => {
+		// make sure we reset tag selection on mount so that we don't accidentally
+		// get an already-active selection into this new tag's state.
+		// TODO: we probably still want to separate tag selection states between
+		// components/use-cases as described in a comment elsewhere
+		resetTagSelection();
+	}, []);
 
 	function onInputChange(e: React.ChangeEvent<HTMLInputElement>) {
 		setNewTag((current) => ({ ...current, [e.target.name]: e.target.value }));
