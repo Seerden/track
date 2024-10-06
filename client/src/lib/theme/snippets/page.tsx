655bd57afccd51c9df9b67d55579ca67184497b8
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-06 12:34:38 +0200

    style: tweak framer page component style a little bit

--- client/src/lib/theme/snippets/page.tsx
+++ client/src/lib/theme/snippets/page.tsx
@@ -3,8 +3,10 @@ import { pageBorder } from "./border";
 import { pageShadow } from "./shadow";
 
 export const pageStyle = css`
-	background-color: #fdfdfd;
-	max-width: 500px;
+	background-color: #efefef;
+	max-width: 800px;
+	display: flex;
+	justify-content: center;
 	margin: 0 auto;
 	margin-top: 1rem;
 	padding: 1.2rem 1.6rem;
