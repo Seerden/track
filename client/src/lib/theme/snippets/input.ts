abb02bd81578147c72d80ab7e7a142d0f1e13d0d
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-06 12:36:09 +0200

    style: extract some input-related styles to a snippet

---
+++ client/src/lib/theme/snippets/input.ts
@@ -0,0 +1,10 @@
+import { css } from "styled-components";
+
+export const inputStyle = css`
+	font-size: 0.94rem;
+	outline: none;
+	border: none;
+	padding: 0.3rem 0.5rem;
+	box-shadow: 0.4rem 0.4rem 0.1rem -0.2rem #ddd;
+	border-radius: 3px;
+`;
