38ae1072cafed0c1e06d362812d3cb596315e542
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-09 13:50:15 +0200

    memoize activities derived from query state

--- client/src/components/Today/use-today.ts
+++ client/src/components/Today/use-today.ts
@@ -1,10 +1,15 @@
 import dayjs from "dayjs";
+import { useMemo } from "react";
 import useActivitiesQuery from "../../lib/use-activities-query";
 import { activityFallsOnDay, assignIndentationLevelToActivities } from "./activity";
 
 export default function useToday() {
 	const { data: activitiesData } = useActivitiesQuery();
-	const activities = Object.values(activitiesData?.activitiesById ?? {});
+	const activities = useMemo(() => {
+		return Object.values(activitiesData?.activitiesById ?? {}); // TODO: should this not be in a useActivities hook or someting?
+	}, [activitiesData]);
+
+	Object.values(activitiesData?.activitiesById ?? {});
 	const today = dayjs.utc();
 	const todayActivities = activities.filter((activity) => {
 		return activityFallsOnDay(activity, today);
