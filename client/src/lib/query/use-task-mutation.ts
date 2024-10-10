321be6179b85ac9dc38fa1f6b0a1dcc19f20d436
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-10 13:53:32 +0200

    implement mutation hook for completion update

---
+++ client/src/lib/query/use-task-mutation.ts
@@ -0,0 +1,23 @@
+import { createRequestConfig } from "@/lib/fetch/create-post-config";
+import { baseUrl } from "@/lib/fetch/fetch-constants";
+import { Data } from "@/types/query.types";
+import { Activity, ActivityUpdateInput } from "@/types/server/activity.types";
+import { useMutation } from "@tanstack/react-query";
+
+async function putTaskCompletion(input: ActivityUpdateInput) {
+	return (
+		await fetch(
+			`${baseUrl}/task/${input.activity_id}/completion`,
+			createRequestConfig.put({ input }),
+		)
+	).json();
+}
+
+export default function useTaskCompletionMutation() {
+	return useMutation<Data<"input", ActivityUpdateInput>, unknown, Activity>({
+		async mutationFn(activity) {
+			return putTaskCompletion(activity);
+		},
+		mutationKey: ["task-completion"],
+	});
+}
