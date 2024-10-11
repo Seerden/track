6f5975fb6fcd57c448f70d160027e28952db047c
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-11 14:49:35 +0200

    use Checkbox component in Task and make it interactive based on task.completed

--- client/src/components/Today/Task.tsx
+++ client/src/components/Today/Task.tsx
@@ -1,3 +1,5 @@
+import useTaskCompletionMutation from "@/lib/query/use-task-mutation";
+import { Checkbox } from "@/lib/theme/components/Checkbox";
 import { activityEnd, activityStart } from "@lib/activity";
 import { filterTagsById } from "@lib/filter-tags";
 import { ActivityWithIds } from "@type/server/activity.types";
@@ -14,9 +16,22 @@ type TaskProps = {
 export default function Task({ activity, tagsById }: TaskProps) {
 	const tags = filterTagsById(activity.tag_ids, tagsById); // TODO: pass this as a prop and, in Tasks, get it from a hook
 
+	const { mutate } = useTaskCompletionMutation();
+	function putCompletion() {
+		mutate({ ...activity, completed: !activity.completed });
+	}
+
 	return (
 		<S.Task>
-			<S.Checkbox type="checkbox" checked={activity.completed} />
+			<S.CheckboxWrapper>
+				<S.Checkbox
+					type="checkbox"
+					style={{ display: "none" }}
+					checked={activity.completed}
+					onChange={putCompletion}
+				/>
+				<Checkbox checked={activity.completed} />
+			</S.CheckboxWrapper>
 			<S.TaskName>{activity.name}</S.TaskName>
 			<S.Times>
 				{activityStart(activity).format("HH:mm")}
