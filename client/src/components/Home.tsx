012c920a349f10f3a779d71483897eaab993cd83
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-14 12:34:03 +0200

    remove unnecessary modal use conditional

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,18 +1,14 @@
-import { useModalState } from "@/lib/state/modal-state";
 import Modal from "./Modal";
 import NewActivity from "./NewActivity/NewActivity";
 import NewNote from "./NewNote/NewNote";
 import Notes from "./Notes/Notes";
 
 function Home() {
-	const { state } = useModalState("home-notes");
 	return (
 		<>
-			{state.isOpen && ( // TODO: I do not want to have to do this, but currently it needs to be, because the listeners do not reset properly without it
-				<Modal initialOpen modalId={"home-notes"}>
-					<Notes />
-				</Modal>
-			)}
+			<Modal initialOpen modalId={"home-notes"}>
+				<Notes />
+			</Modal>
 			<NewNote />
 			<NewActivity />
 		</>
