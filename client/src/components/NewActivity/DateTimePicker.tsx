71fd74ddad6479033d6eb1e531fcbdda088d5b3f
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-15 18:57:25 +0200

    only set datetimepicker time state on blur, not on change

--- client/src/components/NewActivity/DateTimePicker.tsx
+++ client/src/components/NewActivity/DateTimePicker.tsx
@@ -51,7 +51,7 @@ export default function DateTimePicker({ setState }: DateTimePickerProps) {
 						<span>Start time</span>
 						<input
 							type="text"
-							onChange={(e) => onTimeChange(e, "start")}
+							onBlur={(e) => onTimeChange(e, "start")}
 							// TODO: Need something in the UI to clarify the time
 							// format (also in the endTime field), just this
 							// placeholder is not enough -- do this after implementing
@@ -65,7 +65,7 @@ export default function DateTimePicker({ setState }: DateTimePickerProps) {
 						<input
 							type="text"
 							placeholder={"HHmm"}
-							onChange={(e) => onTimeChange(e, "end")}
+							onBlur={(e) => onTimeChange(e, "end")}
 							disabled={allDay}
 						/>
 					</S.Label>
