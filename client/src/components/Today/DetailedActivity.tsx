38d372e1d9999eb35ed16bf11acd019940c8b355
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-15 21:01:26 +0200

    style: more work on DetailedActivity

--- client/src/components/Today/DetailedActivity.tsx
+++ client/src/components/Today/DetailedActivity.tsx
@@ -1,10 +1,13 @@
 import Modal from "@/components/Modal";
-import { activityEnd, activityStart } from "@/lib/activity";
+import S from "@/components/Today/DetailedActivity.style";
+import { activityEnd, activityStart, hasNotEnded, startsInFuture } from "@/lib/activity";
+import { createDate } from "@/lib/datetime/make-date";
 import useActivitiesQuery from "@/lib/query/use-activities-query";
 import useTagsQuery from "@/lib/query/use-tags-query";
+import useTaskCompletionMutation from "@/lib/query/use-task-mutation";
+import { Checkbox } from "@/lib/theme/components/Checkbox";
 import type { Datelike } from "@/types/date.types";
 import type { ActivityWithIds } from "@/types/server/activity.types";
-import dayjs from "dayjs";
 
 type DetailedActivityProps = {
 	id: ActivityWithIds["activity_id"];
@@ -12,75 +15,84 @@ type DetailedActivityProps = {
 
 export const activityModalId = "activity-modal";
 
+// TODO: instead of this, do time (humanizedDate), with a tooltip on
+// humanizedDate that shows the full date.
 function format(date: Datelike) {
-	return dayjs(date).format("HH:mm (YYYY/MM/DD)");
+	return createDate(date).format("HH:mm (YYYY/MM/DD)");
 }
 
 export default function DetailedActivity({ id }: DetailedActivityProps) {
 	const { data: activitiesData } = useActivitiesQuery();
 	const { data: tagsData } = useTagsQuery();
+	const { mutate } = useTaskCompletionMutation();
+
+	// TODO: this should _definitely_ be in a useTaskCompletionMutation hook.
+	// It's being done in Task.tsx as well, and just shouldn't be done at the
+	// component-level like this.
+	function putCompletion(activity: ActivityWithIds) {
+		mutate({ ...activity, completed: !activity.completed });
+	}
 	if (!activitiesData) return null;
 
 	const activity = activitiesData.activitiesById[id];
 
+	const humanizedStart = `${startsInFuture(activity) ? "starts" : "started"} ${activityStart(activity).fromNow()}`;
+	const showHumanizedStart = hasNotEnded(activity);
+
 	return (
 		<Modal modalId={activityModalId}>
-			<section
-				style={{
-					minWidth: "400px"
-				}}
-			>
-				<div
-					style={{
-						fontSize: "1.5rem",
-						fontWeight: "bold",
-						marginBottom: "0.5rem",
-						width: "70%",
-						backgroundColor: "#ddd",
-						color: "white",
-						padding: "0.5rem",
-						borderRadius: "3px 3px 20px 3px"
-					}}
-				>
-					{activity.name}
-				</div>
+			<S.Wrapper>
+				<S.Title>
+					{activity.is_task && (
+						<S.CheckboxWrapper>
+							<input type="checkbox" onChange={() => putCompletion(activity)} />
+							<Checkbox checked={activity.completed} />
+						</S.CheckboxWrapper>
+					)}
+					<span>{activity.name}</span>
+				</S.Title>
 
-				{!!activity.description.length && <div>{activity.description}</div>}
+				{!!activity.description.length && (
+					<S.Description>{activity.description}</S.Description>
+				)}
 
-				<div
-					style={{
-						display: "flex",
-						flexDirection: "column",
-						justifyContent: "flex-end",
-						alignItems: "flex-end",
-						fontSize: "0.8rem",
-						color: "#888"
-					}}
-				>
-					<span>{format(activityStart(activity))}</span>
-					<span>{format(activityEnd(activity))}</span>
-				</div>
+				<S.Time>
+					{showHumanizedStart && (
+						<S.HumanizedStart>{humanizedStart}</S.HumanizedStart>
+					)}
+					<S.Datetime>
+						<span>
+							from <span>{format(activityStart(activity))}</span>
+						</span>
+						<span>
+							to <span>{format(activityEnd(activity))}</span>
+						</span>
+					</S.Datetime>
+				</S.Time>
+
+				{!!activity.is_task && (
+					<S.Task>
+						{activity.completed && (
+							<S.Datetime>
+								{activity.completion_start && (
+									<>from {format(activity.completion_start)}</>
+								)}
+								{activity.completion_end && (
+									<>to {format(activity.completion_end)}</>
+								)}
+							</S.Datetime>
+						)}
+					</S.Task>
+				)}
 
 				{tagsData?.tagsById && (
-					<ul>
+					<S.Tags>
 						{activity.tag_ids.map((id) => (
-							<li
-								key={id}
-								style={{
-									listStyle: "none",
-									padding: "0.5rem 0.9rem",
-									borderRadius: "4px",
-									backgroundColor: "dodgerblue",
-									width: "max-content",
-									color: "azure"
-								}}
-							>
-								{tagsData.tagsById[id].name}
-							</li>
+							<S.Tag key={id}>{tagsData.tagsById[id].name}</S.Tag>
 						))}
-					</ul>
+					</S.Tags>
 				)}
-			</section>
+			</S.Wrapper>
 		</Modal>
 	);
 }
