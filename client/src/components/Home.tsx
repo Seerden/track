06dd4f9525f7b62eb54a0a676fb5e2218909d537
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-21 00:08:16 +0200

    do not log tag tree in Home

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,14 +1,11 @@
+import NewActivity from "@/components/NewActivity/NewActivity";
+import NewNote from "@/components/NewNote/NewNote";
 import TagTree from "@/components/TagTree/TagTree";
 import modalIds from "@/lib/modal-ids";
-import useTagsTreeQuery from "@/lib/query/use-tags-tree-query";
 import Modal from "./Modal";
-import NewActivity from "./NewActivity/NewActivity";
-import NewNote from "./NewNote/NewNote";
 import Notes from "./Notes/Notes";
 
 function Home() {
-	const { data } = useTagsTreeQuery();
-	console.log({ data });
 	return (
 		<>
 			<Modal initialOpen modalId={modalIds.notes.home}>
