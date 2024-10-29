a78d805173b7f93c5abc971af21411140f122856
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-29 10:28:53 +0100

    fix import of style components -- forgot when redoing export syntax

--- client/src/components/Today/Note.tsx
+++ client/src/components/Today/Note.tsx
@@ -3,7 +3,8 @@ import type { NoteWithIds } from "@type/server/note.types";
 import type { TagWithIds } from "@type/server/tag.types";
 import type { ById } from "@type/server/utility.types";
 import TagCard from "../TagCard/TagCard";
-import * as S from "./Today.style";
+import T from "./Tasks.style";
+import S from "./Today.style";
 
 type NoteProps = {
 	note: NoteWithIds;
@@ -17,11 +18,11 @@ export function Note({ note, tagsById }: NoteProps) {
 		<S.Note>
 			{note.title?.length && <S.NoteTitle>{note.title}</S.NoteTitle>}
 			<S.NoteContent>{note.content}</S.NoteContent>
-			<S.Tags>
+			<T.Tags>
 				{tags.map((tag) => (
 					<TagCard key={tag.tag_id} tag={tag} />
 				))}
-			</S.Tags>
+			</T.Tags>
 		</S.Note>
 	);
 }
