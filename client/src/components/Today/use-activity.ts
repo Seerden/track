f43f0f4fb394d2203a2aacc86da4a4e5be4f6075
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-29 16:13:34 +0100

    limit durationHours so that visually, activities always "end" at midnight on the timeline view

--- client/src/components/Today/use-activity.ts
+++ client/src/components/Today/use-activity.ts
@@ -1,13 +1,21 @@
 import type { ActivityProps } from "@/components/Today/Activity";
-import { activityDuration, activityStart } from "@/lib/activity";
+import { activityDuration, activityStart, activityStartHour } from "@/lib/activity";
 import modalIds from "@/lib/modal-ids";
 import { useModalState } from "@/lib/state/modal-state";
 
 type UseActivityProps = ActivityProps;
 
 export default function useActivity({ activity, indentation }: UseActivityProps) {
-	const durationHours = activityDuration(activity);
 	const offset = activityStart(activity).minute() / 60;
+
+	/** This is the _displayed_ duration on the Today timeline. A multiday
+	 * activity still "ends" at midnight on this view. TODO: maybe change this
+	 * variable name to reflect this. */
+	const durationHours = Math.min(
+		activityDuration(activity),
+		24 - offset - activityStartHour(activity, activityStart(activity)),
+	);
+
 	const level = indentation.get(activity.activity_id) ?? 0;
 	const { setModalState } = useModalState(modalIds.detailedActivity);
 
