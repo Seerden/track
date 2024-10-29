e1fb1827e0c4cc8287437b2cd0d9e483779e9e1d
Christiaan Seerden <christiaanseerden@live.nl>
2024-10-29 10:28:05 +0100

    make completion timestamps strings right before insertion

--- server/src/lib/data/update-activity.ts
+++ server/src/lib/data/update-activity.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { sqlConnection } from "src/db/init";
 import { Activity, ActivityUpdateInput } from "types/data/activity.types";
 import { WithSQL } from "types/sql.types";
@@ -6,13 +7,17 @@ export function updateActivityCompletion({
 	sql = sqlConnection,
 	input,
 }: WithSQL<{ input: ActivityUpdateInput }>) {
+	const start = input.completion_start
+		? dayjs(input.completion_start).toISOString()
+		: null;
+	const end = input.completion_end ? dayjs(input.completion_end).toISOString() : null;
 	// Writing the fields out manually is a bit verbose, but it's clearer than
 	// manipulating and using ${input}
 	return sql<[Activity]>`
       update activities 
       set completed = ${input.completed ?? null},
-         completion_start = ${input.completion_start ?? null},
-         completion_end = ${input.completion_end ?? null}
+         completion_start = ${start},
+         completion_end = ${end}
       where activity_id = ${input.activity_id}
       returning *
    `;
