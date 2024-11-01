f56385006e5fefd5392043a3e6bb6a9796c770fc
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-01 03:28:44 +0100

    same as with tasks, pass tags directly to Note instead of passing tagsById and filtering in there

--- client/src/components/Today/Note.tsx
+++ client/src/components/Today/Note.tsx
@@ -1,28 +1,26 @@
-import { filterTagsById } from "@lib/filter-tags";
 import type { NoteWithIds } from "@type/server/note.types";
 import type { TagWithIds } from "@type/server/tag.types";
-import type { ById } from "@type/server/utility.types";
 import TagCard from "../TagCard/TagCard";
 import N from "./style/Notes.style";
 import S from "./style/Today.style";
 
 type NoteProps = {
 	note: NoteWithIds;
-	tagsById?: ById<TagWithIds>;
+	tags: TagWithIds[];
 };
 
-export function Note({ note, tagsById }: NoteProps) {
-	const tags = filterTagsById(note.tag_ids ?? [], tagsById);
-
+export function Note({ note, tags }: NoteProps) {
 	return (
 		<N.Note>
-			{note.title?.length && <N.NoteTitle>{note.title}</N.NoteTitle>}
+			{!!note.title?.length && <N.NoteTitle>{note.title}</N.NoteTitle>}
 			<N.NoteContent>{note.content}</N.NoteContent>
-			<S.Tags>
-				{tags.map((tag) => (
-					<TagCard key={tag.tag_id} tag={tag} />
-				))}
-			</S.Tags>
+			{!!tags.length && (
+				<S.Tags>
+					{tags.map((t) => (
+						<TagCard key={t.tag_id} tag={t} />
+					))}
+				</S.Tags>
+			)}
 		</N.Note>
 	);
 }
