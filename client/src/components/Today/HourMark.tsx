b14e23a1390164d454ff5891e72a7faaf598efe7
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-01 03:25:12 +0100

    types: extract a props type

--- client/src/components/Today/HourMark.tsx
+++ client/src/components/Today/HourMark.tsx
@@ -1,7 +1,11 @@
 import { formatHour } from "@lib/datetime/format-date";
 import H from "./style/HourMark.style";
 
-export default function HourMark({ index }: { index: number }) {
+type HourMarkProps = {
+	index: number;
+};
+
+export default function HourMark({ index }: HourMarkProps) {
 	const label = formatHour(index);
 	return <H.HourMark>{label}</H.HourMark>;
 }
