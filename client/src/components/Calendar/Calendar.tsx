64b65928ff32aa125e5876a3603e109e906472dd
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-05 00:37:31 +0100

    put useCalendar in its own file

--- client/src/components/Calendar/Calendar.tsx
+++ client/src/components/Calendar/Calendar.tsx
@@ -1,59 +1,12 @@
 import type { Row } from "@/components/Calendar/build-calendar-rows";
-import { buildCalendarRows } from "@/components/Calendar/build-calendar-rows";
-import type { CalendarProps, MonthAndYear } from "@/components/Calendar/calendar.types";
+import type { CalendarProps } from "@/components/Calendar/calendar.types";
+import { useCalendar } from "@/components/Calendar/use-calendar";
 import useMonthPicker from "@/components/Calendar/use-month-picker";
-import { toMonthAndYear } from "@/lib/datetime/format-date";
-import { createDate, firstOfTheMonth } from "@/lib/datetime/make-date";
 import { daysOfWeekShort } from "@/lib/datetime/weekdays";
 import type { Maybe } from "@/types/server/utility.types";
 import { MonthPicker } from "@mantine/dates";
 import type { Dayjs } from "dayjs";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
 import S from "./Calendar.style";
-
-type UseCalendarProps = CalendarProps & {
-	setExternalState?: React.Dispatch<React.SetStateAction<Maybe<Dayjs>>>;
-};
-
-function useCalendar({ initialMonth, initialYear, setExternalState }: UseCalendarProps) {
-	const [selectedDate, setSelectedDate] = useState<Maybe<Dayjs>>();
-
-	const [monthAndYear, setMonthAndYear] = useState<MonthAndYear>(() => ({
-		month: initialMonth,
-		year: initialYear
-	}));
-
-	const firstDayOfTheMonth = useMemo(
-		() => firstOfTheMonth(monthAndYear), // TODO: use firstDayOfMonth function here
-		[monthAndYear]
-	);
-
-	const selectDate = useCallback(
-		(day: number) => {
-			setSelectedDate(
-				createDate(new Date(monthAndYear.year, monthAndYear.month, day))
-			);
-		},
-		[monthAndYear, setSelectedDate]
-	);
-
-	useEffect(() => {
-		if (selectedDate) setExternalState?.(selectedDate);
-	}, [selectedDate]);
-
-	const rows = buildCalendarRows(firstDayOfTheMonth);
-
-	const title = useMemo(() => toMonthAndYear(firstDayOfTheMonth), [firstDayOfTheMonth]);
-
-	return {
-		monthAndYear,
-		setMonthAndYear,
-		rows,
-		title,
-		selectDate,
-		selectedDate
-	} as const;
-}
 
 type CalendarRowProps = {
 	month: number;
---
+++ client/src/components/Calendar/use-calendar.ts
@@ -0,0 +1,55 @@
+import { buildCalendarRows } from "@/components/Calendar/build-calendar-rows";
+import type { CalendarProps, MonthAndYear } from "@/components/Calendar/calendar.types";
+import { toMonthAndYear } from "@/lib/datetime/format-date";
+import { createDate, firstOfTheMonth } from "@/lib/datetime/make-date";
+import type { Maybe } from "@/types/server/utility.types";
+import type { Dayjs } from "dayjs";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+type UseCalendarProps = CalendarProps & {
+	setExternalState?: React.Dispatch<React.SetStateAction<Maybe<Dayjs>>>;
+};
+
+export function useCalendar({
+	initialMonth,
+	initialYear,
+	setExternalState,
+}: UseCalendarProps) {
+	const [selectedDate, setSelectedDate] = useState<Maybe<Dayjs>>();
+
+	const [monthAndYear, setMonthAndYear] = useState<MonthAndYear>(() => ({
+		month: initialMonth,
+		year: initialYear,
+	}));
+
+	const firstDayOfTheMonth = useMemo(
+		() => firstOfTheMonth(monthAndYear), // TODO: use firstDayOfMonth function here
+		[monthAndYear],
+	);
+
+	const selectDate = useCallback(
+		(day: number) => {
+			setSelectedDate(
+				createDate(new Date(monthAndYear.year, monthAndYear.month, day)),
+			);
+		},
+		[monthAndYear, setSelectedDate],
+	);
+
+	useEffect(() => {
+		if (selectedDate) setExternalState?.(selectedDate);
+	}, [selectedDate]);
+
+	const rows = buildCalendarRows(firstDayOfTheMonth);
+
+	const title = useMemo(() => toMonthAndYear(firstDayOfTheMonth), [firstDayOfTheMonth]);
+
+	return {
+		monthAndYear,
+		setMonthAndYear,
+		rows,
+		title,
+		selectDate,
+		selectedDate,
+	} as const;
+}
