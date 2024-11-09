cb65e7ad3777c5fa84e26206a9e4a048884de8e4
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-09 21:17:09 +0100

    add modal ids for new note, new activity, new task

--- client/src/lib/modal-ids.ts
+++ client/src/lib/modal-ids.ts
@@ -5,7 +5,12 @@ export default {
 		newTag: "new-tag-tag-selector"
 	},
 	notes: {
-		home: "home-notes"
+		home: "home-notes",
+		new: "new-note"
+	},
+	activities: {
+		new: "new-activity",
+		newTask: "new-task"
 	},
 	detailedActivity: "detailed-activity", // TODO: do we want this to be dependent on activity_id?
 	tagTree: { tree: "tag-tree", tagSelector: "tag-tree-tag-selector" }
