8b9790b443651f6f732b7664f0cea40aa1c286c9
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-09 14:11:47 +0100

    use createDate instead of manually creating dates in compare.ts

--- client/src/lib/datetime/compare.ts
+++ client/src/lib/datetime/compare.ts
@@ -1,11 +1,10 @@
 import type { Datelike } from "@type/date.types";
-import dayjs from "dayjs";
-import { today } from "./make-date";
+import { createDate, today } from "./make-date";
 
 export function sameDay(one: Datelike, two: Datelike) {
-	return dayjs(one).utc().local().isSame(dayjs(two).utc().local(), "day");
+	return createDate(one).isSame(createDate(two), "day");
 }
 
 export function isToday(date: Datelike) {
-	return dayjs(date).utc().local().isSame(today(), "day");
+	return createDate(date).isSame(today(), "day");
 }
