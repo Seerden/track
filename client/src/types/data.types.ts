bc637f8f0b3905d1a8f97d1666b4bbf75f851b2e
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-10 12:14:11 +0100

    move ActivitiesData to data.types

--- client/src/types/data.types.ts
+++ client/src/types/data.types.ts
@@ -1,4 +1,6 @@
+import type { ActivityWithIds } from "@/types/server/activity.types";
 import type { DataById } from "./query.types";
 import type { TagWithIds } from "./server/tag.types";
 
 export type TagsData = DataById<TagWithIds>;
+export type ActivitiesData = DataById<ActivityWithIds>;
