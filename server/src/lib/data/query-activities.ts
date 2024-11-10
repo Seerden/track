5a8bf83ec15cb5e3a2fbd1bc5eac2d4560e3b536
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-10 12:37:50 +0100

    only query the single updated activity from the database after updating

--- server/src/lib/data/query-activities.ts
+++ server/src/lib/data/query-activities.ts
@@ -12,6 +12,25 @@ export async function queryActivitiesByUser({
 	return sql<Activity[]>`select * from activities where user_id = ${user_id}`;
 }
 
+export async function queryActivityById({
+	sql = sqlConnection,
+	activity_id,
+}: WithSQL<{ activity_id: ID }>) {
+	const [activity] = await sql<
+		Activity[]
+	>`select * from activities where activity_id = ${activity_id}`;
+	return activity;
+}
+
+export async function queryTagRelationsForActivity({
+	sql = sqlConnection,
+	activity_id,
+}: WithSQL<{ activity_id: ID }>) {
+	return sql<
+		ActivityTagRelation[]
+	>`select * from activities_tags where activity_id = ${activity_id}`;
+}
+
 export async function queryActivityTagsByUser({
 	sql = sqlConnection,
 	user_id,
@@ -19,6 +38,18 @@ export async function queryActivityTagsByUser({
 	return sql<
 		ActivityTagRelation[]
 	>`select * from activities_tags where user_id = ${user_id}`;
+}
+
+export async function queryActivityByIdWithRelations({
+	sql = sqlConnection,
+	activity_id,
+}: WithSQL<{
+	activity_id: ID;
+}>) {
+	const activity = await queryActivityById({ sql, activity_id });
+	const tagRelations = await queryTagRelationsForActivity({ sql, activity_id });
+	const merged = mergeActivitiesAndRelations([activity], tagRelations);
+	return merged[activity_id];
 }
 
 export async function queryActivitiesAndRelations({
