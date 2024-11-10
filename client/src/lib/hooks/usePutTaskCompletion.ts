e89e825672db5936ce460167416a175cb0786e0f
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-10 12:25:28 +0100

    move onSuccess from usePutTaskCompletion to the mutation hook itself

--- client/src/lib/hooks/usePutTaskCompletion.ts
+++ client/src/lib/hooks/usePutTaskCompletion.ts
@@ -1,40 +1,12 @@
-import { queryClient } from "@/lib/query-client";
 import useTaskCompletionMutation from "@/lib/query/useTaskMutation";
-import type { ActivitiesData } from "@/types/data.types";
 import type { ActivityWithIds } from "@/types/server/activity.types";
 import { useCallback } from "react";
 
 export default function usePutTaskCompletion(task: ActivityWithIds) {
 	const { mutate } = useTaskCompletionMutation();
 
-	function updateActivitiesCache(updatedActivity: ActivityWithIds) {
-		queryClient.setQueryData<ActivitiesData>(["activities"], (old): ActivitiesData => {
-			if (!old)
-				// This should never happen, since how could we update an activity that doesn't exist?
-				return {
-					byId: {
-						[updatedActivity.activity_id]: updatedActivity
-					}
-				};
-
-			if (!updatedActivity) return old;
-
-			return {
-				byId: {
-					...old.byId,
-					[updatedActivity.activity_id]: updatedActivity
-				}
-			};
-		});
-	}
-
 	const putCompletion = useCallback(() => {
-		mutate(
-			{ input: { ...task, completed: !task.completed } },
-			{
-				onSuccess: updateActivitiesCache
-			}
-		);
+		mutate({ input: { ...task, completed: !task.completed } });
 	}, [task, mutate]);
 
 	return putCompletion;
