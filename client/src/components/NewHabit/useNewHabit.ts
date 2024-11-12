78d9f419a9b03ff377ece5a69e460fd5505e8848
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-12 12:44:49 +0100

    wip: functionality hook for NewHabit

---
+++ client/src/components/NewHabit/useNewHabit.ts
@@ -0,0 +1,93 @@
+import { createDate } from "@/lib/datetime/make-date";
+import useAuthentication from "@/lib/hooks/useAuthentication";
+import { useNewHabitMutation } from "@/lib/query/useNewHabitMutation";
+import { useTagSelection } from "@/lib/state/selected-tags-state";
+import type { NewHabit } from "@/types/server/habit.types";
+import { hasValidUserId } from "@/types/server/user-id.guards";
+import type { Nullable } from "@/types/server/utility.types";
+import type { Dayjs } from "dayjs";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+export default function useNewHabit() {
+	const { currentUser } = useAuthentication();
+	const { mutate: submit } = useNewHabitMutation();
+	const { selectedTagIds, resetTagSelection } = useTagSelection();
+
+	useEffect(() => {
+		resetTagSelection();
+	}, []);
+
+	function parseNewHabit(habit: Partial<NewHabit>): NewHabit {
+		// TODO: actually validate habit
+		return habit as NewHabit;
+	}
+
+	const [habit, setHabit] = useState<Omit<NewHabit, "user_id">>({
+		name: "",
+		description: "",
+		start_timestamp: new Date(),
+		end_timestamp: null,
+		frequency: 1,
+		interval: 1,
+		interval_unit: "day",
+		goal_type: "checkbox",
+		goal_unit: null,
+		goal: null
+	});
+
+	const habitWithUserIdField = useMemo(() => {
+		return Object.assign(habit, { user_id: currentUser?.user_id ?? null });
+	}, [habit, currentUser]);
+
+	function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+
+		if (!hasValidUserId(habitWithUserIdField)) return;
+
+		submit({
+			habit: parseNewHabit(habitWithUserIdField),
+			tagIds: selectedTagIds
+		});
+	}
+
+	useEffect(() => {
+		console.log({ habit });
+	}, [habit]);
+
+	const maybePlural = useCallback(
+		(s: string) => {
+			return habit.interval === 1 ? s : s + "s";
+		},
+		[habit.interval]
+	);
+
+	function onInputChange(e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+		const value = e.target.value;
+		let parsedValue: Nullable<string | number | Dayjs> = value;
+		switch (e.target.type) {
+			case "number":
+				parsedValue = +value;
+				break;
+			case "date":
+				parsedValue = createDate(value);
+				break;
+			case "text":
+				parsedValue = value === "" ? null : value;
+				break;
+			case "radio":
+				parsedValue = value as "checkbox" | "goal";
+				break;
+		}
+		setHabit((current) => ({
+			...current,
+			[e.target.name]: parsedValue
+		}));
+	}
+	return {
+		habit,
+		setHabit,
+		maybePlural,
+		onInputChange,
+		onSubmit
+	};
+}
