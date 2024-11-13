4d47f7654705f6a4be255b3418157768554bca1e
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-13 01:46:27 +0100

    (chore) add another mock habit

--- client/src/components/habits/DetailedHabit/mock.ts
+++ client/src/components/habits/DetailedHabit/mock.ts
@@ -17,3 +17,20 @@ export const habit: HabitWithIds = {
 	end_timestamp: createDate(new Date()).add(1, "year"),
 	tag_ids: [40]
 };
+
+export const booleanHabit: HabitWithIds = {
+	user_id: 4,
+	created_at: new Date(),
+	description: "Don't let them fall out",
+	frequency: 2,
+	interval: 1,
+	interval_unit: "day",
+	goal_type: "checkbox",
+	goal: null,
+	goal_unit: null,
+	habit_id: 0,
+	name: "Brush teeth",
+	start_timestamp: new Date(),
+	end_timestamp: null,
+	tag_ids: [41]
+};
