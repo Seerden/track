6010827a28176e2d2211a05af5893443ec2f6f6b
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-15 13:50:52 +0100

    Home now just renders Today

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,36 +1,7 @@
-import DetailedHabit from "@/components/habits/DetailedHabit/DetailedHabit";
-import Habits from "@/components/habits/HabitEntryItem/Habits";
-import { withSyntheticHabitEntries } from "@/components/habits/HabitEntryItem/synthetic";
-import { createDate } from "@/lib/datetime/make-date";
-import useHabitsData from "@/lib/hooks/useHabitsData";
-import type { TimeWindow } from "@/types/time-window.types";
-import { useMemo } from "react";
-
-// TODO this is just a placeholder
-const timeWindow: TimeWindow = {
-	intervalUnit: "day",
-	startDate: createDate(new Date()).startOf("day"),
-	endDate: createDate(new Date()).add(2, "day")
-};
+import Today from "@/components/Today/Today";
 
 function Home() {
-	const { habitsWithEntriesById } = useHabitsData();
-
-	if (!habitsWithEntriesById) return null;
-	const _habits = useMemo(() => {
-		return withSyntheticHabitEntries(habitsWithEntriesById, timeWindow);
-	}, [habitsWithEntriesById, timeWindow]);
-
-	const habitList = Object.values(_habits);
-	if (!habitList.length) return null;
-
-	return (
-		<>
-			<DetailedHabit habit={habitList[0]} />
-			simulating daily view
-			<Habits habits={_habits} />
-		</>
-	);
+	return <Today />;
 }
 
 export default Home;
