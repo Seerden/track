fe5a14b2c24e0bcd2e15d0553d16611e9f0c0856
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-18 18:07:33 +0100

    (chore) whitespace in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import router from "@/lib/router";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
-
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import duration from "dayjs/plugin/duration";
@@ -15,6 +14,7 @@ import { RouterProvider } from "react-router-dom";
 import "./index.scss";
 import { worker } from "./mocks/browser";
 import "./normalize.css";
+
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
 dayjs.extend(tz);
