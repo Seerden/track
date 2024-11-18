3738e747cb73885de2040a604ccbc14a02ded667
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-18 14:01:49 +0100

    tsconfig and vite settings

---
+++ client/vite.config.js
@@ -0,0 +1,22 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "../client/src"),
+            "@lib": path.resolve(__dirname, "../client/src/lib"),
+            "@components": path.resolve(__dirname, "../client/src/components"),
+            "@t": path.resolve(__dirname, "../shared/types")
+        },
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+    },
+    server: {
+        watch: {
+            usePolling: true
+        }
+    }
+});
