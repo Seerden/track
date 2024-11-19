ca2de97095b00727a1dfb427a804dcf680787890
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-19 11:52:18 +0100

    navigate on login/logout

--- client/src/lib/hooks/query/user/useLogoutMutation.ts
+++ client/src/lib/hooks/query/user/useLogoutMutation.ts
@@ -1,16 +1,17 @@
 import { createRequestConfig } from "@/lib/fetch/create-request-config";
 import { baseUrl } from "@/lib/fetch/fetch-constants";
+import useRouteProps from "@/lib/hooks/useRouteProps";
+import { queryClient } from "@/lib/query-client";
 import { mk, qk } from "@/lib/query-keys";
 import { localUser } from "@/lib/user-storage";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 
 async function postLogout() {
 	return (await fetch(`${baseUrl}/auth/logout`, createRequestConfig.post())).json();
 }
 
 export default function useLogoutMutation() {
-	const client = useQueryClient();
-
+	const { navigate } = useRouteProps();
 	return useMutation({
 		mutationKey: mk.user.logout, // TODO: should we distinguish between login and logout?
 		async mutationFn() {
@@ -19,7 +20,13 @@ export default function useLogoutMutation() {
 		onSuccess: () => {
 			// unset local user on successful logout
 			localUser.destroy();
-			client.removeQueries({ queryKey: qk.user.me });
+			queryClient.removeQueries({ queryKey: qk.user.me });
+
+			// TODO: should we clear _everything_? Or should it be enough to clear
+			// `me` (above)? Need to make it so useAuthentication redirects on
+			// logout, too, instead of doing it here, I think.
+			queryClient.clear();
+			navigate("/");
 		}
 	});
 }
