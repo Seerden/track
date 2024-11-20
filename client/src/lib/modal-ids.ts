ca8fd69523adc9b858543ac1d71541dc2b87e5a6
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-20 15:59:22 +0100

    todo: on modal id for detailed activity

--- client/src/lib/modal-ids.ts
+++ client/src/lib/modal-ids.ts
@@ -17,7 +17,7 @@ export default {
 		new: "new-activity",
 		newTask: "new-task"
 	},
-	detailedActivity: "detailed-activity", // TODO: do we want this to be dependent on activity_id?
+	detailedActivity: "detailed-activity", // TODO: should be under activities.detailed like with habits and others
 	tagTree: { tree: "tag-tree", tagSelector: "tag-tree-tag-selector" },
 	tags: {
 		detailed: "detailed-tag"
