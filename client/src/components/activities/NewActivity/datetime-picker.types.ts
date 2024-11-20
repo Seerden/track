b85a9ace2ff3786c7444dffc96c5945580c1390a
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-20 16:32:02 +0100

    types: DateTimeStateSetter

--- client/src/components/activities/NewActivity/datetime-picker.types.ts
+++ client/src/components/activities/NewActivity/datetime-picker.types.ts
@@ -1,6 +1,11 @@
 import type { DateTimeField } from "@/types/form.types";
 import type { Datelike } from "@t/data/utility.types";
 
+export type DateTimeStateSetter = (args: {
+	name: DateTimeField;
+	value: Datelike;
+}) => void;
+
 export type DateTimePickerProps = {
-	setState: (args: { name: DateTimeField; value: Datelike }) => void;
+	setState: DateTimeStateSetter;
 };
