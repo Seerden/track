e57cce21aa6ad237c4fff7c0da4ce200a3d2a211
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-20 16:32:54 +0100

    wip: start preparing useNewActivity for edit functionality

--- client/src/components/activities/NewActivity/useNewActivity.ts
+++ client/src/components/activities/NewActivity/useNewActivity.ts
@@ -1,42 +1,22 @@
+import type { DateTimeStateSetter } from "@/components/activities/NewActivity/datetime-picker.types";
+import type { ModalId } from "@/lib/modal-ids";
 import { queryClient } from "@/lib/query-client";
 import { qk } from "@/lib/query-keys";
 import { useModalState } from "@/lib/state/modal-state";
-import type { DateTimeField } from "@/types/form.types";
 import { useNewActivityMutation } from "@lib/hooks/query/activities/useNewActivityMutation";
 import useAuthentication from "@lib/hooks/useAuthentication";
 import useRouteProps from "@lib/hooks/useRouteProps";
 import { useTagSelection } from "@lib/state/selected-tags-state";
 import type { NewActivity } from "@t/data/activity.types";
 import { hasValidUserId } from "@t/data/user-id.guards";
-import type { Datelike } from "@t/data/utility.types";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { parseNewActivity } from "./parse-new-activity";
 
-export default function useNewActivity({
-	initialIsTask = false,
-	modalId
-}: {
-	initialIsTask?: boolean;
-	modalId?: string;
-}) {
+function useSubmitNewActivity(newActivity: Partial<NewActivity>, modalId?: ModalId) {
 	const { mutate: submit } = useNewActivityMutation();
 	const { navigate } = useRouteProps();
-	const { currentUser } = useAuthentication();
-	const { selectedTagIds, resetTagSelection } = useTagSelection();
+	const { selectedTagIds } = useTagSelection();
 	const { closeModal } = useModalState();
-
-	const [newActivity, setNewActivity] = useState<Partial<NewActivity>>(() => ({
-		name: "",
-		description: "",
-		user_id: currentUser?.user_id,
-		is_task: initialIsTask
-	}));
-
-	useEffect(() => {
-		resetTagSelection();
-	}, []);
-
-	const isTask = useMemo(() => !!newActivity.is_task, [newActivity.is_task]);
 
 	function onSubmit(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault();
@@ -47,16 +27,43 @@ export default function useNewActivity({
 			{ activity: parseNewActivity(newActivity), tagIds: selectedTagIds },
 			{
 				onSuccess: () => {
-					// TODO: put routes in a variable
-					navigate("/today");
+					navigate("/today"); // TODO: put routes in a variable
+
 					queryClient.invalidateQueries({
 						queryKey: qk.activities.all
 					});
+
 					if (modalId) closeModal(modalId);
 				}
 			}
 		);
 	}
+
+	return { onSubmit };
+}
+
+export default function useNewActivity({
+	initialIsTask = false,
+	modalId
+}: {
+	initialIsTask?: boolean;
+	modalId?: ModalId;
+}) {
+	const { currentUser } = useAuthentication();
+	const { resetTagSelection } = useTagSelection();
+
+	const [newActivity, setNewActivity] = useState<Partial<NewActivity>>(() => ({
+		name: "",
+		description: "",
+		user_id: currentUser?.user_id,
+		is_task: initialIsTask
+	}));
+
+	const { onSubmit } = useSubmitNewActivity(newActivity, modalId);
+
+	useEffect(() => {
+		resetTagSelection();
+	}, []);
 
 	function onInputChange(e: React.ChangeEvent<HTMLInputElement>) {
 		const { type, name, value } = e.target;
@@ -69,17 +76,17 @@ export default function useNewActivity({
 
 	// TODO: this is functionally the same as onInputChange, except the typing is
 	// different. Do we need a type for onDateTimeChange from DateTimePicker?
-	function onDateTimeChange({ name, value }: { name: DateTimeField; value: Datelike }) {
+	const onDateTimeChange: DateTimeStateSetter = ({ name, value }) => {
 		setNewActivity((current) => ({
 			...current,
 			[name]: value
 		}));
-	}
+	};
 
 	return {
 		onSubmit,
 		onInputChange,
 		onDateTimeChange,
-		isTask
+		isTask: !!newActivity.is_task
 	};
 }
