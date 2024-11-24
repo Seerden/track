002298acf45a72daa4017c88abc32f751db8c860
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-24 12:33:04 +0100

    add decorators to storybook for mantine, react-query, styled-components

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -12,14 +12,20 @@ function getAbsolutePath(value: string): any {
 const config: StorybookConfig = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 	addons: [
-        getAbsolutePath("@storybook/addon-essentials"),
-        getAbsolutePath("@chromatic-com/storybook"),
-        getAbsolutePath("@storybook/addon-interactions"),
-        getAbsolutePath("@storybook/addon-themes")
-    ],
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@chromatic-com/storybook"),
+		getAbsolutePath("@storybook/addon-interactions"),
+		getAbsolutePath("@storybook/addon-themes")
+	],
 	framework: {
 		name: getAbsolutePath("@storybook/react-vite"),
 		options: {}
-	}
+	},
+	previewHead: (head) => `
+      ${head}
+      <style>
+			@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+		</style>
+   `
 };
 export default config;
---
+++ client/.storybook/preview.tsx
@@ -0,0 +1,48 @@
+import { createTheme, DEFAULT_THEME, MantineProvider } from "@mantine/core";
+import type { Preview } from "@storybook/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../src/lib/theme/theme";
+
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "../src/index.scss";
+import "../src/normalize.css";
+
+const queryClient = new QueryClient();
+
+// TODO: include msw: https://github.com/storybookjs/storybook/issues/12489#issuecomment-702958192
+
+const decorators = [
+	(Story) => (
+		<ThemeProvider theme={theme}>
+			<Story />
+		</ThemeProvider>
+	),
+	(Story) => (
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} position="bottom" />
+			<Story />
+		</QueryClientProvider>
+	),
+	(Story) => (
+		<MantineProvider theme={createTheme(DEFAULT_THEME)}>
+			<Story />
+		</MantineProvider>
+	)
+];
+
+const preview: Preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		}
+	},
+	decorators
+};
+export default preview;
