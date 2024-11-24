81e466bf0b835c7bbfb2ae3a6ce2955530d72188
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-24 15:10:26 +0100

    Buttons barrel file

---
+++ client/src/lib/theme/components/buttons/index.ts
@@ -0,0 +1,11 @@
+import ActionButtons from "@/lib/theme/components/buttons/Action";
+import CellButtons from "@/lib/theme/components/buttons/Cell";
+import SubmitButtons from "@/lib/theme/components/buttons/Submit";
+import UnstyledButton from "@/lib/theme/components/buttons/Unstyled";
+
+export {
+	UnstyledButton as Unstyled,
+	ActionButtons as action,
+	CellButtons as cell,
+	SubmitButtons as submit
+};
