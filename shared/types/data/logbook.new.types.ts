d140c95d35a4418e8ac9540805ccab81bdb1571b
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-25 18:23:03 +0100

    types: New* types for logbook tables

---
+++ shared/types/data/logbook.new.types.ts
@@ -0,0 +1,43 @@
+// Because of the complexity of the feature, I'm not going to build the types up
+// sequentially like with other data types, but instead I'm going to define the
+// partial types after the main type.
+
+import {
+	FieldTemplate,
+	FieldValue,
+	Item,
+	ItemRow,
+	ItemTemplate,
+	Log,
+	Logbook,
+	LogTemplate,
+} from "types/data/logbook.types";
+import { OmitStrict } from "types/data/utility.types";
+
+export type NewLogbook = OmitStrict<Logbook, "logbook_id" | "created_at">;
+
+export type NewLogTemplate = OmitStrict<
+	LogTemplate,
+	"log_template_id" | "created_at"
+>;
+
+export type NewLog = OmitStrict<Log, "log_id" | "created_at">;
+
+export type NewItemTemplate = OmitStrict<
+	ItemTemplate,
+	"item_template_id" | "created_at"
+>;
+
+export type NewItem = OmitStrict<Item, "item_id" | "created_at">;
+
+export type NewItemRow = OmitStrict<ItemRow, "item_row_id" | "created_at">;
+
+export type NewFieldTemplate = OmitStrict<
+	FieldTemplate,
+	"field_template_id" | "created_at"
+>;
+
+export type NewFieldValue = OmitStrict<
+	FieldValue,
+	"created_at" | "field_value_id"
+>;
