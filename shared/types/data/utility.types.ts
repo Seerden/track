55d682bf6ec8b5de1160a23d31b5bcfb619195ef
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-25 18:22:47 +0100

    types: OmitStrict type

--- shared/types/data/utility.types.ts
+++ shared/types/data/utility.types.ts
@@ -41,3 +41,10 @@ export type StartAndEnd = {
 	start: Dayjs;
 	end: Dayjs;
 };
+
+/**
+ * Omit that only works with keys actually present in the object.
+ * @see https://github.com/microsoft/TypeScript/issues/30825#issuecomment-673002409 */
+export type OmitStrict<T, K extends keyof T> = T extends any
+	? Pick<T, Exclude<keyof T, K>>
+	: never;
