199faec51c07276e2a25aba27278fc67a8878e25
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-26 12:55:33 +0100

    fix import path and tweak New* types a bit

--- shared/types/data/logbook.new.types.ts
+++ shared/types/data/logbook.new.types.ts
@@ -11,8 +11,8 @@ import {
 	Log,
 	Logbook,
 	LogTemplate,
-} from "types/data/logbook.types";
-import { OmitStrict } from "types/data/utility.types";
+} from "./logbook.types";
+import { OmitStrict } from "./utility.types";
 
 export type NewLogbook = OmitStrict<Logbook, "logbook_id" | "created_at">;
 
@@ -34,10 +34,22 @@ export type NewItemRow = OmitStrict<ItemRow, "item_row_id" | "created_at">;
 
 export type NewFieldTemplate = OmitStrict<
 	FieldTemplate,
+	"field_template_id" | "created_at" | "item_template_id"
+>;
+
+/** NewFieldTemplate with item_template_id */
+export type NewFieldTemplateWithId = OmitStrict<
+	FieldTemplate,
 	"field_template_id" | "created_at"
 >;
 
 export type NewFieldValue = OmitStrict<
+	FieldValue,
+	"created_at" | "field_value_id" | "item_row_id"
+>;
+
+/** NewFieldValue with item_row_id */
+export type NewFieldValueWithId = OmitStrict<
 	FieldValue,
 	"created_at" | "field_value_id"
 >;
