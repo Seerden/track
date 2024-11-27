936ade54834f6bd8765c0542bc99ca9c12b63625
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-27 11:26:31 +0100

    abstract fetch functions into an api object

---
+++ client/src/lib/fetch/api.ts
@@ -0,0 +1,49 @@
+import { createRequestConfig } from "@/lib/fetch/create-request-config";
+import { makeAuthorizedUrl } from "@/lib/fetch/make-authorized-url";
+
+async function apiGet<T>({ url }: { url: string }): Promise<T> {
+	const _url = makeAuthorizedUrl(url);
+	const response: Promise<T> = (
+		await fetch(_url, {
+			credentials: "include",
+			method: "GET"
+		})
+	).json();
+
+	return response;
+}
+
+function apiUpdate(method: "put" | "post") {
+	const configFunction = createRequestConfig[method];
+
+	return async <TInput, TResponse>({
+		url,
+		body
+	}: {
+		url: string;
+		body: TInput;
+	}): Promise<TResponse> => {
+		const _url = makeAuthorizedUrl(url);
+		const response: Promise<TResponse> = (
+			await fetch(_url, configFunction({ body }))
+		).json();
+		return response;
+	};
+}
+
+const apiPost = apiUpdate("post");
+const apiPut = apiUpdate("put");
+
+function apiDelete<T>({ url }: { url: string }): Promise<T> {
+	const _url = makeAuthorizedUrl(url);
+	return (async () => (await fetch(_url, createRequestConfig.delete())).json())();
+}
+
+const api = {
+	get: apiGet,
+	post: apiPost,
+	put: apiPut,
+	delete: apiDelete
+};
+
+export default api;
