75355c259b9ef791fa7233abda99195a92538615
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-27 14:09:40 +0100

    clean up a file

--- server/src/lib/data/models/logbooks/insert-field-template.ts
+++ server/src/lib/data/models/logbooks/insert-field-template.ts
@@ -1,18 +1,9 @@
 import { sqlConnection } from "@/db/init";
-import type {
-   FieldTemplate,
-   NewFieldTemplate,
-   NewFieldTemplateWithId,
-} from "@t/data/logbook.types";
+import type { FieldTemplate, NewFieldTemplateWithId } from "@t/data/logbook.types";
 import type { QueryFunction } from "types/sql.types";
 
-type B = NewFieldTemplate;
-type C = NewFieldTemplateWithId;
-
 export const insertFieldTemplate: QueryFunction<
-	{
-		newFieldTemplate: NewFieldTemplateWithId;
-	},
+	{ newFieldTemplate: NewFieldTemplateWithId },
 	Promise<FieldTemplate>
 > = async ({ sql = sqlConnection, newFieldTemplate }) => {
 	const [insertedFieldTemplate] = await sql<[FieldTemplate]>`
