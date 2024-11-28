8e36b72d5c31b9882128f8b3976eac30b4383f11
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-28 01:19:37 +0100

    experimental request handlers for log card

--- client/src/components/logbooks/Logbooks/LogSection.tsx
+++ client/src/components/logbooks/Logbooks/LogSection.tsx
@@ -1,7 +1,18 @@
 // LogSection represents an item and all of its rows.
 // It displays the item name and a list of ItemRowListItems.
 
-export default function LogSection() {
+import { ItemRowListItem } from "@/components/logbooks/Logbooks/LogCard";
+import type { FieldTemplate, FieldValue, Item, ItemRow } from "@t/data/logbook.types";
+
+type Field = {
+	template: FieldTemplate;
+	value: FieldValue;
+};
+type Row = ItemRow & { item: Item } & { fields: Field[] };
+type Rows = Row[];
+type Section = Rows[];
+
+export default function LogSection({ section }: { section: Section }) {
 	return (
 		<section>
 			<h1>{item.name}</h1>
--- server/src/lib/data/request-handlers/get/get-logs.ts
+++ server/src/lib/data/request-handlers/get/get-logs.ts
@@ -1,8 +1,15 @@
 import { groupById } from "@/lib/data/models/group-by-id";
+import { queryFieldTemplatesByItemTemplate } from "@/lib/data/models/logbooks/query-field-templates";
+import { queryFieldValuesByLog } from "@/lib/data/models/logbooks/query-field-values";
+import { queryItemRowsByLog } from "@/lib/data/models/logbooks/query-item-rows";
+import { queryItemTemplateById } from "@/lib/data/models/logbooks/query-item-templates";
+import { queryItemById } from "@/lib/data/models/logbooks/query-items";
 import {
+	queryLogById,
 	queryLogsByLogbook,
 	queryLogsByUser,
 } from "@/lib/data/models/logbooks/query-logs";
+import type { FieldTemplate, FieldValue, Item, ItemRow } from "@t/data/logbook.types";
 import type { RequestHandler } from "express";
 
 /** Queries all logs by user. */
@@ -25,4 +32,78 @@ export const getLogsByLogbook: RequestHandler = async (req, res) => {
 	const byId = groupById(logs, "log_id");
 
 	res.json({ byId });
+};
+
+type Field = {
+	template: FieldTemplate;
+	value: FieldValue;
+};
+type Row = ItemRow & { item: Item } & { fields: Field[] };
+type Rows = Row[];
+type Section = Rows[];
+
+// build the entire set of data that we need to display a log card in the
+// client. I'm not sure if this is how we actually want to do it, but it's an
+// experiment to see how I feel about keeping client-side logic to a minimum.
+export const getLogCardById: RequestHandler = async (req, res) => {
+	const log_id = +req.params.log_id;
+
+	const log = await queryLogById({ log_id });
+	const itemRows = await queryItemRowsByLog({ log_id });
+	const fieldValues = await queryFieldValuesByLog({ log_id });
+	const uniqueItemIds = Array.from(new Set(itemRows.map((row) => row.item_id)));
+	const items = await Promise.all(
+		uniqueItemIds.map(async (item_id) => queryItemById({ item_id })),
+	);
+	const uniqueItemTemplateIds = Array.from(
+		new Set(items.map((item) => item.item_template_id)),
+	);
+	const itemTemplates = await Promise.all(
+		uniqueItemTemplateIds.map(async (item_template_id) =>
+			queryItemTemplateById({ item_template_id }),
+		),
+	);
+	const uniqueItemTemplates = Array.from(new Set(itemTemplates));
+
+	// Each section corresponds to an item template and all of the items that
+	// descend from it along with their filled-out fields.
+	const sections = await Promise.all(
+		uniqueItemTemplates.map(async ({ item_template_id }) => {
+			const _items = items.filter((i) => i.item_template_id === item_template_id);
+			const fieldTemplates = await queryFieldTemplatesByItemTemplate({
+				item_template_id,
+			});
+
+			// Find item rows for each item, and include the fields for each row.
+			return _items.map((item) => {
+				const _itemRows = itemRows.filter(
+					(itemRow) => item.item_id === itemRow.item_id,
+				);
+
+				return _itemRows.map((ir) => {
+					const _fieldValues = fieldValues.filter(
+						(fv) => fv.item_row_id === ir.item_row_id,
+					);
+
+					const fields = fieldTemplates.map((ft) => ({
+						template: ft,
+						value: _fieldValues.find(
+							(fv) => fv.field_template_id === ft.field_template_id,
+						) as FieldValue,
+					})) as Field[];
+
+					return {
+						...ir,
+						item,
+						fields,
+					};
+				});
+			});
+		}),
+	);
+
+	res.json({
+		log,
+		sections,
+	});
 };
