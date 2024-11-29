7825cef19f2f45e95d783374c451459faaecb215
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-30 00:33:36 +0100

    [WIP] partial implementation of initial LogbookForm component

---
+++ client/src/components/logbooks/LogbookForm/LogbookForm.style.ts
@@ -0,0 +1,56 @@
+import { Button, Title } from "@/components/logbooks/LogDetail/style/_common.style";
+import containers from "@/lib/theme/components/container.style";
+import { font } from "@/lib/theme/font";
+import { flex } from "@/lib/theme/snippets/flex";
+import { spacing } from "@/lib/theme/snippets/spacing";
+import styled from "styled-components";
+
+const Form = styled.form`
+	${containers.minimal};
+
+	width: max-content;
+`;
+
+const FormTitle = styled(Title)`
+	display: flex;
+	align-items: center;
+	gap: 1rem;
+	padding: 1rem 2rem;
+	padding-bottom: 0.2rem;
+	margin-bottom: 1rem;
+`;
+
+const Label = styled.label`
+	${flex.column};
+	background-color: #fff;
+	padding: 0.4rem;
+	border-radius: 6px;
+	box-shadow: 0 0.2rem 0.3rem 0 #aaa;
+
+	span {
+		font-size: ${font.size["0.9"]};
+		color: #333;
+		padding-left: 0.5rem;
+	}
+
+	input,
+	textarea {
+		resize: none;
+		margin: 0.3rem;
+		font-size: ${font.size["0.93"]};
+		border: none;
+		line-height: 1.5rem;
+		background-color: #fff;
+		padding: 0.3rem 0.6rem;
+		border: 1px solid #ddd;
+		border-radius: 3px;
+	}
+`;
+
+const Submit = styled(Button)`
+	color: #eee;
+	margin-top: 1rem;
+	${spacing.padding.wide({ size: 1, ratio: 1.5 })}
+`;
+
+export default { Form, FormTitle, Label, Submit };
--- client/src/components/logbooks/LogbookForm/useLogbookForm.ts
+++ client/src/components/logbooks/LogbookForm/useLogbookForm.ts
@@ -1,20 +1,61 @@
+import useMutateLogbook from "@/lib/hooks/query/logbooks/useMutateLogbook";
+import useMutateNewLogbook from "@/lib/hooks/query/logbooks/useMutateNewLogbook";
+import useAuthentication from "@/lib/hooks/useAuthentication";
 import type { NewLogbook } from "@t/data/logbook.new.types";
+import type { Logbook } from "@t/data/logbook.types";
 import { useState } from "react";
 
-export default function useLogbookForm() {
-	const { mutate } = useMutateNewLogbook();
-	const [logbook, setLogbook] = useState<NewLogbook>({
-		description: null,
-		name: "",
-		user_id: -1
-	});
+type NewLogbookState = Omit<NewLogbook, "user_id">;
 
-	function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+const defaultLogbook: NewLogbookState = {
+	description: null,
+	name: ""
+};
+
+function isLogbook(logbook: Logbook | Omit<NewLogbook, "user_id">): logbook is Logbook {
+	return (logbook as Logbook).logbook_id !== undefined;
+}
+
+export default function useLogbookForm({
+	logbook: existingLogbook
+}: {
+	logbook?: Logbook;
+}) {
+	const { currentUser } = useAuthentication();
+
+	const { mutate: submitNewLogbook } = useMutateNewLogbook();
+	const { mutate: submitUpdatedLogbook } = useMutateLogbook();
+	const [logbook, setLogbook] = useState<Logbook | NewLogbookState>(
+		existingLogbook ?? defaultLogbook
+	);
+
+	function handleInputChange(
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) {
 		setLogbook((current) => ({
 			...current,
 			[e.target.name]: e.target.value
 		}));
 	}
 
-	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {}
+	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+
+		if (!currentUser) return;
+
+		if (!isLogbook(logbook)) {
+			const logbookWithUserId: NewLogbook = {
+				...logbook,
+				user_id: currentUser.user_id
+			};
+			submitNewLogbook({ newLogbook: logbookWithUserId });
+		} else {
+			submitUpdatedLogbook({ logbook });
+		}
+	}
+
+	return {
+		handleInputChange,
+		handleSubmit
+	};
 }
