173cfa9940212d1b2c4808ded74e1f1b26b19758
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-30 00:30:47 +0100

    [WIP] extract common-to-logbook-feature Button and Title styled components

--- client/src/components/logbooks/LogDetail/style/_common.style.ts
+++ client/src/components/logbooks/LogDetail/style/_common.style.ts
@@ -1,3 +1,5 @@
+import { Action } from "@/lib/theme/components/buttons";
+import { font } from "@/lib/theme/font";
 import { spacing } from "@/lib/theme/snippets/spacing";
 import styled, { css } from "styled-components";
 
@@ -83,4 +85,32 @@ export const FieldWrapper = styled.div<{ small?: boolean }>`
 	border-radius: 5px;
 	outline: 2px solid #ddd;
 	margin: 0.3rem;
+`;
+
+export const Button = styled(Action.Default)<{ $iconPosition: "left" | "right" }>`
+	width: max-content;
+	border-radius: 10px;
+	margin-left: 1rem;
+	padding: 1.5rem 2.5rem;
+	${(p) =>
+		p.$iconPosition === "left"
+			? css`
+					padding-left: 1.5rem;
+				`
+			: css`
+					padding-right: 1.5rem;
+				`}
+	color: white;
+	display: flex;
+	gap: 1rem;
+`;
+
+// eslint-disable-next-line track/no-direct-styled-import
+export const Title = styled.h1`
+	font-size: ${font.size["2"]};
+	border-radius: 6px;
+	padding-inline: 1rem;
+	margin: 0;
+	box-shadow: 0 0.5rem 1.5rem -0.6rem #ccc;
+	border-bottom: 3px solid #fff;
 `;
