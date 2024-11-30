443fcefb9b4be0d7653a5da67ccbda78718dd2e2
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-30 19:39:03 +0100

    fix input shape to match new client-side api body shape

--- server/src/lib/data/request-handlers/put/put-habit-entry.ts
+++ server/src/lib/data/request-handlers/put/put-habit-entry.ts
@@ -3,7 +3,7 @@ import type { HabitEntryUpdateInput } from "@t/data/habit.types";
 import type { RequestHandler } from "express";
 
 const putHabitEntry: RequestHandler = async (req, res) => {
-	const { input } = req.body as { input: HabitEntryUpdateInput };
+	const input = req.body as HabitEntryUpdateInput;
 	const [habitEntry] = await updateHabitEntry({ input });
 	res.json({ habitEntry });
 };
--- server/src/lib/data/request-handlers/put/put-task.ts
+++ server/src/lib/data/request-handlers/put/put-task.ts
@@ -4,7 +4,7 @@ import type { TaskUpdateInput } from "@t/data/activity.types";
 import type { RequestHandler } from "express";
 
 const putTaskCompletion: RequestHandler = async (req, res) => {
-	const { input } = req.body as { input: TaskUpdateInput };
+	const input = req.body as TaskUpdateInput;
 	const [activity] = await updateActivityCompletion({ input });
 	const updatedActivity = await queryActivityByIdWithRelations({
 		activity_id: activity.activity_id,
