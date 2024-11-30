bfee37aa977b5f565fc34a29ed04bad34598b564
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-30 15:29:48 +0100

    initial implementation of basic SelectionList component

---
+++ client/src/components/utility/selection/SelectionList/SelectionList.tsx
@@ -0,0 +1,68 @@
+import Badge from "@/lib/theme/components/Badge";
+import { useReducer } from "react";
+import type { CSS } from "styled-components/dist/types";
+import S from "./SelectionList.style";
+
+type ItemValue = string | number;
+
+type Item = {
+	label: string;
+	value: ItemValue;
+};
+
+type SelectionListProps = {
+	multiple?: boolean;
+	items: Item[];
+	highlightColor?: CSS.Properties["color"];
+	onChange: (values: (string | number)[]) => void;
+};
+
+export default function SelectionList({
+	multiple,
+	items,
+	onChange,
+	highlightColor = "limegreen"
+}: SelectionListProps) {
+	function selectionReducer(selection: ItemValue[], action: { value: ItemValue }) {
+		const valuesSet = new Set(selection);
+
+		if (multiple) {
+			if (valuesSet.has(action.value)) {
+				valuesSet.delete(action.value);
+			} else {
+				valuesSet.add(action.value);
+			}
+		} else {
+			if (valuesSet.has(action.value)) {
+				valuesSet.clear();
+			} else {
+				valuesSet.clear();
+				valuesSet.add(action.value);
+			}
+		}
+
+		const newSelection = Array.from(valuesSet);
+		onChange(newSelection);
+		return newSelection;
+	}
+
+	const [selection, dispatch] = useReducer(selectionReducer, []);
+
+	return (
+		<S.List>
+			{items.map((item) => (
+				<div
+					key={item.label}
+					onClick={() => dispatch({ value: item.value })}
+					style={{
+						cursor: "pointer"
+					}}
+				>
+					<Badge color={selection.includes(item.value) ? highlightColor : "#ccc"}>
+						{`${item.value}`}
+					</Badge>
+				</div>
+			))}
+		</S.List>
+	);
+}
