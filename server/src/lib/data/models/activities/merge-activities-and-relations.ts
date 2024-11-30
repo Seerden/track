cb55479f7d5d863666ad7ad43edda27544d598d6
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-30 21:38:21 +0100

    (chore) codestyle tweak to groupById and mergeActivitiesAndRelations

--- server/src/lib/data/models/activities/merge-activities-and-relations.ts
+++ server/src/lib/data/models/activities/merge-activities-and-relations.ts
@@ -1,3 +1,4 @@
+import { groupById } from "@/lib/data/models/group-by-id";
 import type { Activity, ActivityWithIds } from "@t/data/activity.types";
 import type { ActivityTagRelation } from "@t/data/relational.types";
 import type { ById } from "@t/data/utility.types";
@@ -6,14 +7,15 @@ export function mergeActivitiesAndRelations(
 	activities: Activity[],
 	activityTagRelations: ActivityTagRelation[],
 ) {
-	const activitiesById = {} as ById<ActivityWithIds>;
+	const activitiesById = groupById(activities, "activity_id");
+	const withTagIds = Object.assign({}, activitiesById) as ById<ActivityWithIds>;
 	for (const activity of activities) {
-		activitiesById[activity.activity_id] = Object.assign(activity, { tag_ids: [] });
+		withTagIds[activity.activity_id] = Object.assign({}, activity, { tag_ids: [] });
 	}
 
 	for (const { activity_id, tag_id } of activityTagRelations) {
-		activitiesById[activity_id]?.tag_ids?.push(tag_id);
+		withTagIds[activity_id]?.tag_ids?.push(tag_id);
 	}
 
-	return activitiesById;
+	return withTagIds;
 }
