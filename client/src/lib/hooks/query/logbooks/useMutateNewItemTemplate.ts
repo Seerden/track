55efba340b20782bbd398074c0cc882d3502ed02
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-02 22:32:50 +0100

    invalidate itemTemplates query on item template submit

--- client/src/lib/hooks/query/logbooks/useMutateNewItemTemplate.ts
+++ client/src/lib/hooks/query/logbooks/useMutateNewItemTemplate.ts
@@ -1,5 +1,6 @@
 import api from "@/lib/fetch/api";
-import { mk } from "@/lib/query-keys";
+import { queryClient } from "@/lib/query-client";
+import { mk, qk } from "@/lib/query-keys";
 import type { NewFieldTemplate, NewItemTemplate } from "@t/data/logbook.new.types";
 import type { ItemTemplateAndFieldTemplates } from "@t/data/logbook.types";
 import { useMutation } from "@tanstack/react-query";
@@ -21,6 +22,11 @@ export default function useMutateNewItemTemplate() {
 		async mutationFn(itemTemplateInput) {
 			return postNewItemTemplate(itemTemplateInput);
 		},
-		mutationKey: mk.logbooks.itemTemplate.new
+		mutationKey: mk.logbooks.itemTemplate.new,
+		onSuccess: (data) => {
+			queryClient.invalidateQueries({
+				queryKey: qk.itemTemplates.byLogbook(data.itemTemplate.logbook_id)
+			});
+		}
 	});
 }
