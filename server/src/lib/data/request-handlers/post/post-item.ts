c54717e273c6668b53c931c1ea05433d8f76c424
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-04 16:52:53 +0100

    use input types in post handlers

--- server/src/lib/data/request-handlers/post/post-item.ts
+++ server/src/lib/data/request-handlers/post/post-item.ts
@@ -1,10 +1,10 @@
 import { insertItem } from "@/lib/data/models/logbooks/insert-item";
-import type { NewItem } from "@t/data/logbook.types";
+import type { NewItemInput } from "@t/data/logbook.types";
 import type { RequestHandler } from "express";
 
 /** Request handler for `/data/logbook/item`. */
 export const postItem: RequestHandler = async (req, res) => {
-	const { newItem } = req.body as { newItem: NewItem };
+	const { newItem } = req.body as NewItemInput;
 
 	const item = await insertItem({ newItem });
 
