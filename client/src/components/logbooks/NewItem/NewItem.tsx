5358a3385b10584bd9ad4e16ef0d088097dfb415
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-06 01:12:59 +0100

    (chore) refactor NewItem

--- client/src/components/logbooks/NewItem/NewItem.tsx
+++ client/src/components/logbooks/NewItem/NewItem.tsx
@@ -1,16 +1,10 @@
 import F from "@/components/logbooks/LogbookForm/style/LogbookForm.style";
+import useNewItem from "@/components/logbooks/NewItem/useNewItem";
 import MiniField from "@/components/logbooks/NewItemTemplate/MiniField";
-import useMutateNewItem from "@/lib/hooks/query/logbooks/useMutateNewItem";
-import useQueryFields from "@/lib/hooks/query/logbooks/useQueryFields";
-import type { ModalId } from "@/lib/modal-ids";
-import modalIds from "@/lib/modal-ids";
-import { queryClient } from "@/lib/query-client";
-import { qk } from "@/lib/query-keys";
-import { useModalState } from "@/lib/state/modal-state";
-import type { ItemTemplate, NewItem } from "@t/data/logbook.types";
+import type { ItemTemplate } from "@t/data/logbook.types";
 import type { ID } from "@t/data/utility.types";
 import { LucideNotebookText, LucideNotepadTextDashed } from "lucide-react";
-import { useState } from "react";
+import S from "./style/NewItem.style";
 
 type NewItemProps = {
 	logbook_id: ID;
@@ -20,62 +14,23 @@ type NewItemProps = {
 /** A NewItem form just displays the item template's fields, and allows the user
  *to specify a name. */
 export default function NewItem({ itemTemplate, logbook_id }: NewItemProps) {
-	const { data: fieldsData } = useQueryFields();
-	const [item, setItem] = useState<NewItem>({
-		logbook_id,
-		item_template_id: itemTemplate.item_template_id,
-		name: ""
-	});
+	const {
+		isProbablySuspended,
+		item,
+		handleInputChange,
+		handleSubmit,
+		fieldsForItemTemplate
+	} = useNewItem({ itemTemplate, logbook_id });
 
-	const { mutate: submit } = useMutateNewItem();
-	const { closeModal } = useModalState();
-	const modalId = `${modalIds.logbooks.item.new}-${itemTemplate.name}` as ModalId; // TODO: we use this in ItemSection, so it has to be in sync. We shouuld maek the template id part of hte value.
-
-	function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-		setItem((cur) => ({ ...cur, [e.target.name]: e.target.value }));
-	}
-
-	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-		e.preventDefault();
-
-		submit(
-			{ newItem: item },
-			{
-				onSuccess: () => {
-					closeModal(modalId);
-					// TODO: use a more fine-grained query key to invalidate
-					queryClient.invalidateQueries({ queryKey: qk.logbooks.all, exact: false });
-				}
-			}
-		);
-	}
-
-	if (!fieldsData) return null;
-	const fieldsForItemTemplate = Object.values(fieldsData.byId).filter(
-		// TODO: a case where we _have to_ parse an id to a number because as a
-		// bigint, it comes in as a string -- see
-		// https://github.com/Seerden/track/issues/175
-		(field) => +field.item_template_id === +itemTemplate.item_template_id
-	);
+	if (isProbablySuspended) return null;
 
 	return (
 		<F.Form onSubmit={handleSubmit}>
 			<F.FormTitle style={{ marginBottom: "1rem", paddingBottom: "0.5rem" }}>
 				<LucideNotebookText size={40} color="royalblue" /> New Named{" "}
-				<span
-					style={{
-						backgroundColor: "gold",
-						padding: "0.25rem 0.5rem",
-						borderRadius: "5px",
-						color: "black",
-						boxShadow: "0 1rem 0 -0.75rem goldenrod"
-					}}
-				>
-					{itemTemplate.name}
-				</span>
+				<S.TextHighlight>{itemTemplate.name}</S.TextHighlight>
 			</F.FormTitle>
 			<p>
-				{" "}
 				The <span style={{ color: "blue " }}>{itemTemplate.name}</span> template
 				contains the following fields:
 			</p>
