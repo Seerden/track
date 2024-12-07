0c2366cdd87b9e4bab572ad82fbbdda55ef17d45
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-07 14:37:06 +0100

    functionality hook for LogbookCard

---
+++ client/src/components/logbooks/Logbooks/useLogbookCard.ts
@@ -0,0 +1,28 @@
+import { useQueryLogbookById } from "@/lib/hooks/query/logbooks/useQueryLogbooks";
+import { useQueryLogsByLogbook } from "@/lib/hooks/query/logbooks/useQueryLogs";
+import useRouteProps from "@/lib/hooks/useRouteProps";
+import type { ID } from "@t/data/utility.types";
+
+export default function useLogbookCard({ logbook_id }: { logbook_id?: ID }) {
+	const { params, navigate } = useRouteProps();
+	const logbookId = +(logbook_id ?? (params.logbookId || 0));
+	const { data: logbook } = useQueryLogbookById(logbookId);
+	const { data: logsData } = useQueryLogsByLogbook(logbookId);
+
+	const isProbablySuspended = !logsData || !logbook;
+
+	if (isProbablySuspended) {
+		return {
+			isProbablySuspended
+		};
+	}
+
+	const logs = Object.values(logsData.byId);
+
+	return {
+		isProbablySuspended,
+		logbook,
+		logs,
+		navigate
+	};
+}
