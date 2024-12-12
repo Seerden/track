0507ee712b8cad47a131d439d809cea981789ab3
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-12 14:45:46 +0100

    remove obsolete todo

--- client/src/components/habits/Habits/Habit.tsx
+++ client/src/components/habits/Habits/Habit.tsx
@@ -25,15 +25,6 @@ export default function Habit({ habit }: HabitProps) {
 			// TODO: make keyboard-interactable with tabIndex, onFocus and onBlur
 			onMouseEnter={() => setShowExpand(true)}
 			onMouseLeave={() => setShowExpand(false)}
-			onClick={(e) => {
-				// TODO: this onClick also triggers when the user clicks a child
-				// element, like a checkbox. We don't want that. In Task, we have an
-				// e.stopPropagation() on the checkbox wrapper to prevent this. We'd
-				// also need to do the same to the slider wrapper.. bit hacky, maybe
-				// just trigger modal open on button click instead of the whole item.
-				e.stopPropagation();
-				// openDetailedItemModal(habit.habit_id);
-			}}
 		>
 			<L.ItemName>{habit.name}</L.ItemName>
 			<L.Info>{frequencyString(habit)}</L.Info>
