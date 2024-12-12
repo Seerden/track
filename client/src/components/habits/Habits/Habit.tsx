10eba3165a1f5a0de5775ea9a340d3f6f8b65cc4
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-12 14:43:53 +0100

    expand button for Habits

--- client/src/components/habits/Habits/Habit.tsx
+++ client/src/components/habits/Habits/Habit.tsx
@@ -1,7 +1,11 @@
 import Completion from "@/components/habits/Habits/Completion";
 import { frequencyString } from "@/components/habits/Habits/frequency-string";
+import useDetailedItemModal from "@/lib/hooks/useDetailedItemModal";
+import modalIds from "@/lib/modal-ids";
 import L from "@/lib/theme/components/List.style";
 import type { HabitWithPossiblySyntheticEntries } from "@t/data/habit.types";
+import { LucideMaximize2 } from "lucide-react";
+import { useState } from "react";
 import S from "./style/Habit.style";
 
 type HabitProps = {
@@ -10,13 +14,17 @@ type HabitProps = {
 
 export default function Habit({ habit }: HabitProps) {
 	// const { mutate } = useHabitDeleteMutation();
-	// const { openDetailedItemModal } = useDetailedItemModal(
-	// 	"habit",
-	// 	modalIds.habits.detailed
-	// );
+	const { openDetailedItemModal } = useDetailedItemModal(
+		"habit",
+		modalIds.habits.detailed
+	);
+	const [showExpand, setShowExpand] = useState(false);
 
 	return (
 		<S.Wrapper
+			// TODO: make keyboard-interactable with tabIndex, onFocus and onBlur
+			onMouseEnter={() => setShowExpand(true)}
+			onMouseLeave={() => setShowExpand(false)}
 			onClick={(e) => {
 				// TODO: this onClick also triggers when the user clicks a child
 				// element, like a checkbox. We don't want that. In Task, we have an
@@ -32,6 +40,16 @@ export default function Habit({ habit }: HabitProps) {
 			<S.CompletionWrapper>
 				<Completion habit={habit} entries={habit.entries} />
 			</S.CompletionWrapper>
+			{showExpand && (
+				<S.ExpandButton
+					onClick={(e) => {
+						e.stopPropagation();
+						openDetailedItemModal(habit.habit_id);
+					}}
+				>
+					<LucideMaximize2 size={15} />
+				</S.ExpandButton>
+			)}
 		</S.Wrapper>
 	);
 }
--- client/src/components/habits/Habits/style/Habit.style.ts
+++ client/src/components/habits/Habits/style/Habit.style.ts
@@ -1,9 +1,11 @@
+import { Unstyled } from "@/lib/theme/components/buttons";
 import ListStyle from "@/lib/theme/components/List.style";
 import { flex } from "@/lib/theme/snippets/flex";
 import styled from "styled-components";
 
 const Wrapper = styled(ListStyle.Item)`
-	max-width: 500px; // TODO: this is temporary, needs to become responsive
+	max-width: 500px; /* TODO: this is temporary, needs to become responsive */
+	cursor: unset; /* Because the default ListItem has cursor: pointer. */
 `;
 
 const CompletionWrapper = styled.div`
@@ -16,7 +18,34 @@ const CompletionWrapper = styled.div`
 	min-width: 150px;
 `;
 
+const ExpandButton = styled(Unstyled)<{ $size?: number }>`
+	--size: ${(p) => p.$size ?? 30}px;
+	width: var(--size);
+	height: var(--size);
+
+	border-radius: 50%;
+	justify-self: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	background-color: #fff;
+
+	transition: all 40ms linear;
+	&:hover {
+		svg {
+			color: ${(p) => p.theme.colors.blue.main};
+			transform: scale(1.2);
+		}
+	}
+
+	svg {
+		color: #333;
+	}
+`;
+
 export default {
 	Wrapper,
-	CompletionWrapper
+	CompletionWrapper,
+	ExpandButton
 };
