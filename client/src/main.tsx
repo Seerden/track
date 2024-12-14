065c69650c86834584c4cf05d8105d8d3fdb52e5
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-14 01:02:26 +0100

    initialize Sentry in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,6 +8,23 @@ import "./index.scss";
 import { worker } from "./mocks/browser";
 import "./normalize.css";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+	// Note: we could also use an environment variable, but the DSN is public
+	// anyway and we're probably not going to use different DSNs for different
+	// environments anyway.
+	dsn: "https://b0e7e65134d60f61d0dda728b3c918be@o4508463516680192.ingest.de.sentry.io/4508463529984080",
+	integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+	// Tracing
+	tracesSampleRate: 1.0, //  Capture 100% of the transactions
+	// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+	tracePropagationTargets: ["localhost"], // /^https:\/\/yourserver\.io\/api/],
+	// Session Replay
+	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 if (process.env.NODE_ENV === "development") {
 	await worker.start();
 }
