394f8677d0f46e66e1e120e2907455a020b6a6b6
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-23 15:11:48 +0100

    (chore) remove a boilerplate story comment

--- client/src/components/logbooks/MiniLogTemplate/stories/MiniLogTemplate.stories.tsx
+++ client/src/components/logbooks/MiniLogTemplate/stories/MiniLogTemplate.stories.tsx
@@ -1,7 +1,6 @@
 import MiniLogTemplate from "@/components/logbooks/MiniLogTemplate/MiniLogTemplate";
 import type { Meta, StoryFn } from "@storybook/react";
 
-//👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof MiniLogTemplate> = {
 	component: MiniLogTemplate,
 	title: "components/logbooks/MiniLogTemplate"
