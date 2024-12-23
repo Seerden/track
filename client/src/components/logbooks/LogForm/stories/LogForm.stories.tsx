de275d98bf24a4cd42e90955734cab2a38a57df5
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-23 15:12:50 +0100

    (chore) remove a boilerplate story comment

--- client/src/components/logbooks/LogForm/stories/LogForm.stories.tsx
+++ client/src/components/logbooks/LogForm/stories/LogForm.stories.tsx
@@ -1,7 +1,6 @@
 import LogForm from "@/components/logbooks/LogForm/LogForm";
 import type { Meta, StoryFn } from "@storybook/react";
 
-//👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof LogForm> = {
 	component: LogForm,
 	title: "components/logbooks/LogForm"
