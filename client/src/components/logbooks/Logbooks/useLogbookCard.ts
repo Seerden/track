a1b0040aecd4780ea2786c573035ab3c59402452
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-24 00:24:21 +0100

    (todo) on invalid assumption in a case of isProbablySuspended

--- client/src/components/logbooks/Logbooks/useLogbookCard.ts
+++ client/src/components/logbooks/Logbooks/useLogbookCard.ts
@@ -6,9 +6,12 @@ import type { ID } from "@t/data/utility.types";
 export default function useLogbookCard({ logbook_id }: { logbook_id?: ID }) {
 	const { params } = useRouteProps();
 	const logbookId = +(logbook_id ?? (params.logbookId || 0));
-	const { data: logbook } = useQueryLogbookById(logbookId);
-	const { data: logsData } = useQueryLogsByLogbook(logbookId);
+	const { data: logbook } = useQueryLogbookById(+logbookId);
+	const { data: logsData } = useQueryLogsByLogbook(+logbookId);
 
+	// TODO: !logbook can actually be a valid case when the user tries to go to a
+	// logbook page that doesn't exist. So, instead, use the isSuccess flag or
+	// similar.
 	const isProbablySuspended = !logsData || !logbook;
 
 	if (isProbablySuspended) {
