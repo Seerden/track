b8ae67c58bc09ce3da1ffe4e39517f087cd8b5d2
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-27 14:50:51 +0100

    (refactor) introduce a useLogDetailSectionData subhook

--- client/src/components/logbooks/LogDetail/LogDetailSection.tsx
+++ client/src/components/logbooks/LogDetail/LogDetailSection.tsx
@@ -3,12 +3,8 @@ import ItemSection from "@/components/logbooks/LogDetail/ItemSection";
 import { getRowsForItem } from "@/components/logbooks/LogDetail/lib/get-rows";
 import NewItem from "@/components/logbooks/NewItem/NewItem";
 import Modal from "@/components/utility/Modal/Modal";
-import { useQueryItemsByItemTemplate } from "@/lib/hooks/query/logbooks/useQueryItems";
-import useQueryLogs from "@/lib/hooks/query/logbooks/useQueryLogs";
-import { useQueryLogTemplatesByLogbook } from "@/lib/hooks/query/logbooks/useQueryLogTemplates";
 import type { ItemTemplate } from "@t/data/logbook.types";
 import type { ID } from "@t/data/utility.types";
-import { useMemo, useState } from "react";
 import S from "./style/LogDetailSection.style";
 
 export type LogDetailSectionProps = {
@@ -22,58 +18,22 @@ export default function LogDetailSection({
 	log_id,
 	logbook_id
 }: LogDetailSectionProps) {
-	const { isProbablySuspended, modalId, handleModalOpen, itemRows } =
-		useLogDetailSection({
-			itemTemplate,
-			log_id
-		});
-
-	const { data: itemsData } = useQueryItemsByItemTemplate(itemTemplate.item_template_id);
-	const { data: logTemplatesData } = useQueryLogTemplatesByLogbook(logbook_id);
-	const { data: logsData } = useQueryLogs();
-
-	const itemsById = itemsData?.byId;
-	const items = itemsById ? Object.values(itemsById) : [];
-	const log = logsData?.byId[log_id];
-	const logTemplate = log?.log_template_id
-		? logTemplatesData?.byId[log.log_template_id]
-		: null;
-
-	const layout = logTemplate?.layout;
-	const [manuallySelectedItemIds, setManuallySelectedItemIds] = useState<ID[]>([]);
-
-	const selectedItemIds = useMemo(() => {
-		// TODO: this should be done at a higher level in the component tree.
-		// Determine which sections (and which items) to render before we even get
-		// to this ItemSection component.
-		return items
-			.map((item) => +item.item_id)
-			.filter((id) => {
-				// return true if the item is in the log's log_template.layout
-				// TODO: change this logic once we definitively change the shape of
-				// logTemplate.layout.
-				if (layout?.flat().includes(+id)) return true;
-
-				// return true if there is at least 1 item row for this item in the log (=
-				// in `itemRows`)
-				if (itemRows?.some((row) => +row.item_id === +id)) return true;
-
-				// return true if the user manually selected the item to be in the log
-				// using the not-yet-implemented button
-				return manuallySelectedItemIds.includes(+id);
-			});
-	}, [items, layout, itemRows, manuallySelectedItemIds]);
-
-	// TODO: here, filter out any items that are neither in the log's
-	// log_template, nor have item rows associated with them for this log.
-	const filteredItems = items.filter((item) => selectedItemIds.includes(+item.item_id));
-
-	const notYetSelectedItems = items.filter(
-		(item) => !selectedItemIds.includes(+item.item_id)
-	);
-	const [selectedOption, setSelectedOption] = useState<ID>(
-		+notYetSelectedItems[0]?.item_id
-	);
+	const {
+		isProbablySuspended,
+		modalId,
+		handleModalOpen,
+		itemRows,
+		items,
+		filteredItems,
+		notYetSelectedItems,
+		setSelectedOption,
+		setManuallySelectedItemIds,
+		selectedOption
+	} = useLogDetailSection({
+		itemTemplate,
+		log_id,
+		logbook_id
+	});
 
 	if (isProbablySuspended) return null;
 
@@ -86,7 +46,7 @@ export default function LogDetailSection({
 					<div>This item template does not have any items yet. Create one...</div>
 				)}
 
-				{filteredItems.map((item) => (
+				{filteredItems?.map((item) => (
 					<ItemSection
 						log_id={log_id}
 						key={item.item_id}
@@ -106,7 +66,7 @@ export default function LogDetailSection({
 					<LucidePencilLine />
 					<span style={{ fontWeight: 600 }}>Add {itemTemplate.name}</span>
 				</Action.WithIcon> */}
-				{notYetSelectedItems.length > 0 && (
+				{!!notYetSelectedItems && notYetSelectedItems.length > 0 && (
 					<div>
 						add another "{itemTemplate.name}" item to this log
 						<select
--- client/src/components/logbooks/LogDetail/hooks/useLogDetailSection.ts
+++ client/src/components/logbooks/LogDetail/hooks/useLogDetailSection.ts
@@ -1,28 +1,69 @@
-import { useQueryItemRowsByLog } from "@/lib/hooks/query/logbooks/useQueryItemRows";
+import useLogDetailSectionData from "@/components/logbooks/LogDetail/hooks/useLogDetailSectionData";
 import type { ModalId } from "@/lib/modal-ids";
 import modalIds from "@/lib/modal-ids";
 import { useModalState } from "@/lib/state/modal-state";
 import type { ItemTemplate } from "@t/data/logbook.types";
 import type { ID } from "@t/data/utility.types";
+import { useMemo, useState } from "react";
 
 export default function useLogDetailSection({
 	itemTemplate,
+	logbook_id,
 	log_id
 }: {
 	itemTemplate: ItemTemplate;
+	logbook_id: ID;
 	log_id: ID;
 }) {
-	const { data: itemRowsData } = useQueryItemRowsByLog({ log_id });
+	const { isProbablySuspended, itemRows, items, layout } = useLogDetailSectionData({
+		log_id,
+		logbook_id,
+		item_template_id: itemTemplate.item_template_id
+	});
+
 	const modalId = modalIds.logbooks.item.new(itemTemplate.name) as ModalId;
-
 	const { openModal } = useModalState();
-
 	function handleModalOpen(e: React.MouseEvent<HTMLButtonElement>) {
 		e.preventDefault();
 		openModal(modalId);
 	}
 
-	const isProbablySuspended = !itemRowsData;
+	const [manuallySelectedItemIds, setManuallySelectedItemIds] = useState<ID[]>([]);
+
+	const selectedItemIds = useMemo(() => {
+		// TODO: this should be done at a higher level in the component tree.
+		// Determine which sections (and which items) to render before we even get
+		// to this ItemSection component.
+		return items
+			?.map((item) => +item.item_id)
+			.filter((id) => {
+				// return true if the item is in the log's log_template.layout
+				// TODO: change this logic once we definitively change the shape of
+				// logTemplate.layout.
+				if (layout?.flat().includes(+id)) return true;
+
+				// return true if there is at least 1 item row for this item in the log (=
+				// in `itemRows`)
+				if (itemRows?.some((row) => +row.item_id === +id)) return true;
+
+				// return true if the user manually selected the item to be in the log
+				// using the not-yet-implemented button
+				return manuallySelectedItemIds.includes(+id);
+			});
+	}, [items, layout, itemRows, manuallySelectedItemIds]);
+
+	// TODO: here, filter out any items that are neither in the log's
+	// log_template, nor have item rows associated with them for this log.
+	const filteredItems = items?.filter((item) =>
+		selectedItemIds?.includes(+item.item_id)
+	);
+
+	const notYetSelectedItems = items?.filter(
+		(item) => !selectedItemIds?.includes(+item.item_id)
+	);
+	const [selectedOption, setSelectedOption] = useState<ID>(
+		+(notYetSelectedItems?.[0]?.item_id ?? 0) // TODO: do not use 0
+	);
 
 	if (isProbablySuspended) {
 		return {
@@ -32,8 +73,14 @@ export default function useLogDetailSection({
 
 	return {
 		isProbablySuspended,
-		itemRows: Object.values(itemRowsData.byId),
+		itemRows,
 		modalId,
-		handleModalOpen
+		handleModalOpen,
+		items,
+		filteredItems,
+		notYetSelectedItems,
+		setSelectedOption,
+		setManuallySelectedItemIds,
+		selectedOption
 	};
 }
