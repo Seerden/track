3be910a3435da7b8042f04f9c8edc41d4615c332
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-29 16:34:34 +0100

    (chore) remove a useless div

--- client/src/components/logbooks/LogDetail/ItemSection.tsx
+++ client/src/components/logbooks/LogDetail/ItemSection.tsx
@@ -25,28 +25,26 @@ export default function ItemSection({ rows, item, log_id }: ItemSectionProps) {
 	return (
 		<S.Wrapper>
 			<S.ItemName>{item.name}</S.ItemName>
-			<div>
-				<S.Table>
-					<S.TableContent $columns={labels.length}>
-						<ItemSectionHeader labels={labels} />
-						{rows.map(({ item_row_id }, index) => (
-							<MaybeItemRow
-								key={index}
-								fields={fieldsForItem}
-								item_row_id={item_row_id}
-								index={index}
-							/>
-						))}
-						<NewItemRow
-							log_id={log_id}
-							key={rows.length + 1}
-							position={rows.length + 1}
-							item={item}
-							fieldTemplates={fieldsForItem}
+			<S.Table>
+				<S.TableContent $columns={labels.length}>
+					<ItemSectionHeader labels={labels} />
+					{rows.map(({ item_row_id }, index) => (
+						<MaybeItemRow
+							key={index}
+							fields={fieldsForItem}
+							item_row_id={item_row_id}
+							index={index}
 						/>
-					</S.TableContent>
-				</S.Table>
-			</div>
+					))}
+					<NewItemRow
+						log_id={log_id}
+						key={rows.length + 1}
+						position={rows.length + 1}
+						item={item}
+						fieldTemplates={fieldsForItem}
+					/>
+				</S.TableContent>
+			</S.Table>
 		</S.Wrapper>
 	);
 }
