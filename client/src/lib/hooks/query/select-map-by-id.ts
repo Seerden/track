43fe0509c8313e4d452b1b3220ed60f296a75813
Christiaan Seerden <christiaanseerden@live.nl>
2024-12-31 01:28:44 +0100

    (fix,types) proper MappedData type -- it did not infer the data type properly before

--- client/src/lib/hooks/query/select-map-by-id.ts
+++ client/src/lib/hooks/query/select-map-by-id.ts
@@ -1,8 +1,23 @@
-import type { DataById, DataByIdMap } from "@/types/query.types";
+import type { DataById, MappedData } from "@/types/query.types";
 
-export default function transformByIdToMap<T extends DataById<object>>(
+// export default function transformByIdToMap<T extends DataById<unknown>>(
+// 	data: T
+// ): MappedData<T> {
+// 	type ValueType = T extends DataById<infer U> ? U : never;
+// 	const byId = new Map(Object.entries(data.byId) as [string, ValueType][]);
+// 	return Object.assign({}, data, { byId });
+// }
+
+/** Transform the byId object of a DataById object to a Map.
+ * @note the solution above means we don't have to cast the return value, but
+ * because it still casts the Object.entries(), it's not more type-safe than
+ * the solution below, which is simpler in my opinion. I'm leaving it up there
+ * because I think it's a cool example of `infer`.
+ */
+export default function transformByIdToMap<T extends DataById<unknown>>(
 	data: T
-): DataByIdMap<T> {
+): MappedData<T> {
 	const byId = new Map(Object.entries(data.byId));
-	return Object.assign({}, data, { byId }) as DataByIdMap<T>;
+
+	return Object.assign({}, data, { byId }) as MappedData<T>;
 }
--- client/src/types/query.types.ts
+++ client/src/types/query.types.ts
@@ -1,8 +1,10 @@
-import type { ById, ID } from "@t/data/utility.types";
+import type { ById } from "@t/data/utility.types";
 
 /** Why do I use this in other projects? This is just an object. */
 export type Data<K extends string, T> = Record<K, T>;
 
 export type DataById<T> = Data<"byId", ById<T>>;
 
-export type DataByIdMap<T> = DataById<T> & { byId: Map<ID | string, T> };
+export type MappedData<T extends DataById<unknown>> = {
+	byId: Map<string, T extends DataById<infer U> ? U : never>;
+};
