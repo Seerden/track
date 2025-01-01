6a3efd700cdfbf450c49bc446a94b34dcf22b5bd
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-01 13:23:18 +0100

    (todo) on the "bail" part of getUserIdFromSessionOrBail

--- server/src/lib/data/request-handlers/get-user-id-from-session-or-bail.ts
+++ server/src/lib/data/request-handlers/get-user-id-from-session-or-bail.ts
@@ -1,5 +1,12 @@
 import type { Request, Response } from "express";
 
+/**
+ * @note Maybe we should leave the "bail" part of this to the call site,
+ * because, since this thing isn't actually middleware, the typing isn't robust
+ * enough to know not to pass on to the rest of the logic, so when we forget to
+ * wrap, at the call site, the query handler with an if (user_id) ..., we could
+ * get a "cannot send headers after being sent ..." error.
+ */
 export const getUserIdFromSessionOrBail = (req: Request, res: Response) => {
 	const user_id = req.session.user?.user_id;
 
