0721061ea8b69fab6d5a01f7d1984d7abbb64355
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-05 13:33:59 +0100

    group endpoints for activities

---
+++ shared/src/lib/endpoints/activities-endpoints.ts
@@ -0,0 +1,50 @@
+import { ServerSubService, SubService } from "./endpoint.types";
+
+type ActivitySubService = "activities" | "recurrence";
+
+type ActivityService = Record<ActivitySubService, SubService>;
+export type ServerActivityService = Record<
+	ActivitySubService,
+	ServerSubService
+>;
+
+const activities = {
+	get: {
+		getByUser: "/activities", // getActivities
+	},
+	post: {
+		post: "/", // postActivity
+	},
+	put: {
+		putActivity: "/:activity_id", // putActivity
+		putTaskCompletion: "/task/completion", // putTaskCompletion
+	},
+	delete: {},
+} satisfies SubService;
+
+const recurrence = {
+	get: {
+		getRecurrencesByUser: "/recur",
+		getOccurrencesByUser: "/recur/occurrences",
+		getRecurrenceByActivity: "/recur/activity/:activity_id/recurrence",
+		getOccurrencesByRecurrence: "/recur/:recurrence_id/occurrences",
+	},
+	post: {
+		postRecurrence: "/recur/recurrence",
+		postOccurrence: "/recur/occurrence",
+	},
+	put: {
+		putRecurrence: "/recur/recurrence/:recurrence_id",
+		putOccurrence: "/recur/occurrence/:occurrence_id",
+	},
+	delete: {
+		deleteRecurrence: "/recur/recurrence/:recurrence_id",
+		deleteOccurrence: "/recur/occurrence/:occurrence_id",
+	},
+} satisfies SubService;
+
+// base: data/activities
+export const activityEndpointsService = {
+	activities,
+	recurrence,
+} satisfies ActivityService;
