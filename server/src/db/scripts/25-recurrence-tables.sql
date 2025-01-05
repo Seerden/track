66d66727b0427300a8090d49e0e23af1a5821399
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-05 12:41:51 +0100

    only add column if it not exists

--- server/src/db/scripts/25-recurrence-tables.sql
+++ server/src/db/scripts/25-recurrence-tables.sql
@@ -22,5 +22,5 @@ CREATE TABLE IF NOT EXISTS occurrences (
 );
 
 ALTER TABLE activities 
-ADD column recurrence_id bigint references recurrences,
-ADD column occurrence int check(occurrence >= 0);
+ADD column IF NOT EXISTS recurrence_id bigint references recurrences,
+ADD column IF NOT EXISTS occurrence int check(occurrence >= 0);
