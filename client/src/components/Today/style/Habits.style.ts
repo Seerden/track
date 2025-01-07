cccb61fdf9828c64e31b9c135fa8445e7d385bce
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-07 17:55:58 +0100

    (style) remove Habit-card styling

--- client/src/components/Today/style/Habits.style.ts
+++ client/src/components/Today/style/Habits.style.ts
@@ -1,57 +1,8 @@
-import HabitStyle from "@/components/Today/style/Habit.style";
-import ListStyle from "@/lib/theme/components/List.style";
-import { font } from "@/lib/theme/font";
-import { spacing } from "@/lib/theme/snippets/spacing";
 import styled from "styled-components";
 
 const Habits = styled.div`
 	grid-area: habits;
-	/* the Habits component is an ItemList, and each Habit is an Item, so we can
-   tweak the styles if we target those styled components. */
-	${ListStyle.ItemList} {
-		gap: 0.2rem;
-
-		* {
-			font-size: ${font.size["0.8"]};
-		}
-
-		${ListStyle.Item} {
-			border-radius: 2px;
-			border: 1px solid white;
-
-			box-shadow:
-				0 0rem 0.3rem 0 #ccc,
-				0 0 0.3rem 0 #ddd;
-			background-color: #eee;
-
-			// TODO: I want this to exactly match the spacing of AllDayActivityList
-			${spacing.padding.wide({ size: 0.3, ratio: 3 })}
-
-			${ListStyle.ItemName} {
-				box-shadow: 0 0 0.1rem 0 #777;
-				border: 1px solid white;
-			}
-
-			${ListStyle.Info}, label {
-				/* color: white; */
-			}
-
-			${HabitStyle.CompletionWrapper} {
-				background-color: #eee;
-				${spacing.padding.wide({ size: 0.1, ratio: 3 })};
-				border-radius: 3px;
-
-				span {
-					color: black;
-				}
-
-				// slider background mantine
-				* {
-					--slider-track-bg: #d5d5d5;
-				}
-			}
-		}
-	}
+	--slider-track-bg: #efefef;
 `;
 
 export default {
--- client/src/components/habits/Habits/style/Completion.style.ts
+++ client/src/components/habits/Habits/style/Completion.style.ts
@@ -1,13 +1,13 @@
+import { flex } from "@/lib/theme/snippets/flex";
 import styled from "styled-components";
 
 const List = styled.ul<{ $itemCount: number }>`
 	width: 100%;
-	display: grid;
 	gap: 0.8rem;
-	grid-template-columns: repeat(
-		${(p) => p.$itemCount},
-		calc(100% / ${(p) => p.$itemCount} - 0.8rem)
-	);
+
+	${flex.row};
+
+	flex-wrap: nowrap;
 `;
 
 export default { List };
