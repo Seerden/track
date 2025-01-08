828faa09556112f41b4abfeaab1515117db59501
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-08 18:50:59 +0100

    (wip)

--- client/src/components/auth/Login/Login.tsx
+++ client/src/components/auth/Login/Login.tsx
@@ -1,5 +1,6 @@
 import F from "@/lib/theme/components/form/form.alternate.style";
 import { LucideArrowRight, LucideEye, LucideEyeOff } from "lucide-react";
+import { Link } from "react-router-dom";
 import S from "../style/auth.style";
 import useLogin from "./useLogin";
 
@@ -11,7 +12,7 @@ function Login() {
 		// TODO: this probably becomes a modal that expands from the navigation/header
 		<S.Wrapper>
 			<F.Form onSubmit={handleSubmit}>
-				<F.FormTitle>Login</F.FormTitle>
+				<F.FormTitle>Log in</F.FormTitle>
 				<S.Fields>
 					<F.Label>
 						<span>username</span>
@@ -46,9 +47,26 @@ function Login() {
 						</div>
 					</S.PasswordLabel>
 				</S.Fields>
-				<S.Submit $color="theme">
-					log in <LucideArrowRight size={15} color="black" strokeWidth={2} />
-				</S.Submit>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "space-between",
+						alignItems: "center"
+					}}
+				>
+					<S.Submit $color="theme">
+						log in <LucideArrowRight size={15} color="black" strokeWidth={2} />
+					</S.Submit>
+					<Link
+						to="/register"
+						style={{
+							marginTop: "1rem"
+						}}
+					>
+						register
+					</Link>
+				</div>
 			</F.Form>
 		</S.Wrapper>
 	);
