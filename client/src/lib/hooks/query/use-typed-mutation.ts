8da5015a6e01a96cf036a132df9cd8a32ff345b3
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-11 21:35:48 +0100

    docstring for useMutation

--- client/src/lib/hooks/query/use-typed-mutation.ts
+++ client/src/lib/hooks/query/use-typed-mutation.ts
@@ -1,6 +1,9 @@
 import type { MutationFunction } from "@tanstack/react-query";
 import { useMutation as _useMutation } from "@tanstack/react-query";
 
+/** Use this along with typing to create mutation hooks with proper typing.
+ * @see useMutateRecurrence.ts
+ */
 export default function useMutation<TData, TVariables>(
 	mutationFn: MutationFunction<TData, TVariables>,
 	onSuccess?: () => void
