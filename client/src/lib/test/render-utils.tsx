a3d590a83e41a884a54e4957d8160f2f59ae9f66
Christiaan Seerden <christiaanseerden@live.nl>
2025-06-22 13:08:01 +0200

    (wip) BROKEN tanstack-router testing setup

--- client/src/lib/test/render-utils.tsx
+++ client/src/lib/test/render-utils.tsx
@@ -1,22 +1,17 @@
-import { QueryClientProvider } from "@tanstack/react-query";
 import "@testing-library/jest-dom/vitest";
 import type { RenderOptions } from "@testing-library/react";
 import { render } from "@testing-library/react";
 import type { ReactElement, ReactNode } from "react";
-import { MemoryRouter } from "react-router";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
-import { queryClient } from "../query-client";
 import { theme } from "../theme/theme";
 
+// TODO: this does not contain router provider or query client provider. I'm
+// still working on figuring out the best way to set that up.
 const WithProviders = ({ children }: { children: ReactNode }) => {
 	return (
 		<RecoilRoot>
-			<ThemeProvider theme={theme}>
-				<MemoryRouter>
-					<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-				</MemoryRouter>
-			</ThemeProvider>
+			<ThemeProvider theme={theme}>{children}</ThemeProvider>
 		</RecoilRoot>
 	);
 };
