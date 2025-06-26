5a1328ac7921da2941d4234173703064ba0c2377
Christiaan Seerden <christiaanseerden@live.nl>
2025-06-26 22:04:45 +0200

    set up trpc

---
+++ client/src/lib/trpc.ts
@@ -0,0 +1,40 @@
+import { queryClient } from "@/lib/query-client";
+import type { AppRouter } from "@server/lib/trpc";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import { createTRPCContext, createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
+import SuperJSON from "superjson";
+
+const DOMAIN = import.meta.env.DOMAIN ?? "localhost";
+const HOST = import.meta.env.NODE_ENV === "production" ? DOMAIN : "localhost";
+const url = `http://${HOST}:5000/api/trpc`;
+
+export const trpcReactQuery: ReturnType<typeof createTRPCContext<AppRouter>> =
+	createTRPCContext<AppRouter>();
+
+export const trpcClient: ReturnType<typeof createTRPCClient<AppRouter>> =
+	createTRPCClient<AppRouter>({
+		links: [
+			httpBatchLink({
+				transformer: SuperJSON,
+				url,
+				fetch(url, options) {
+					return fetch(url, {
+						...options,
+						credentials: "include"
+					});
+				}
+			})
+		]
+	});
+
+/**
+ * Have a bug with type inference, even though our tsconfig is otherwise set up
+ * perfectly fine. Using the type annotation as below, makes the error go away.
+ * @see https://github.com/pnpm/pnpm/issues/6089
+ * @see https://github.com/microsoft/TypeScript/issues/42873#issuecomment-1416128545
+ */
+export const trpc: ReturnType<typeof createTRPCOptionsProxy<AppRouter>> =
+	createTRPCOptionsProxy<AppRouter>({
+		client: trpcClient,
+		queryClient: queryClient
+	});
---
+++ server/src/lib/trpc.ts
@@ -0,0 +1,46 @@
+import { sqlConnection } from "@/db/init";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import superjson from "superjson";
+import { z } from "zod";
+import { publicProcedure } from "./trpc/procedures/public.procedure";
+import { t } from "./trpc/trpc-context";
+
+export const appRouter = t.router({
+	hello: publicProcedure
+		.input(z.object({ name: z.string() }))
+		.query(({ input, ctx }) => {
+			return { message: `Hello, ${input.name}!` };
+		}),
+	bye: publicProcedure.input(z.object({ name: z.string() })).query(({ input, ctx }) => {
+		return { message: `Hello, ${input.name}!` };
+	}),
+	dbTest: publicProcedure.query(async () => {
+		{
+			// const result = await pingDatabase();
+			return {
+				sql: sqlConnection,
+				db: await sqlConnection`select array[1]`,
+				env: process.env,
+				who: "am i",
+			};
+		}
+	}),
+	auth: {},
+});
+
+export type AppRouter = typeof appRouter;
+
+export const proxyClient = createTRPCClient<AppRouter>({
+	links: [
+		httpBatchLink({
+			transformer: superjson,
+			url: "http://localhost:5000/api/trpc", // TODO: needs to match what we use in the client, I guess
+			fetch(url, options) {
+				return fetch(url, {
+					...options,
+					credentials: "include",
+				});
+			},
+		}),
+	],
+});
