1870457cce074496710b2b404f0531b1764fa31e
Christiaan Seerden <christiaanseerden@live.nl>
2025-07-19 16:39:37 +0200

    capitalize days of week in sql check

--- server/src/db/scripts/28-recurrence-interval-selection-fields.sql
+++ server/src/db/scripts/28-recurrence-interval-selection-fields.sql
@@ -1,6 +1,6 @@
 ALTER TABLE IF EXISTS recurrences
 ADD column IF NOT EXISTS weekdays text[] CHECK (
-   weekdays <@ ARRAY['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+   weekdays <@ ARRAY['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 ),
 ADD column IF NOT EXISTS monthdays int[] CHECK (
    0 < all(monthdays) AND 32 > all(monthdays)
