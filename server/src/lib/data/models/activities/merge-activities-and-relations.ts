013e64e04189ac8a5c456cacdd889b976843f190
Christiaan Seerden <christiaanseerden@live.nl>
2025-07-23 11:43:58 +0200

    fix type import

--- server/src/lib/data/models/activities/merge-activities-and-relations.ts
+++ server/src/lib/data/models/activities/merge-activities-and-relations.ts
@@ -1,5 +1,5 @@
 import { groupById } from "@/lib/data/models/group-by-id";
-import type { Activity, ActivityWithIds } from "@shared/types/data/activity.types";
+import type { Activity, ActivityWithIds } from "@shared/lib/schemas/activity";
 import type { ActivityTagRelation } from "@shared/types/data/relational.types";
 import type { ById } from "@shared/types/data/utility.types";
 
