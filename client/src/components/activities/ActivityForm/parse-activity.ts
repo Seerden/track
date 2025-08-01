48c4b8500ff4d3eaf9e39bc0df68816b722820fc
Christiaan Seerden <christiaanseerden@live.nl>
2025-08-01 11:48:14 +0200

    remove old new activity validation -- superseded by zod schemas

--- client/src/components/activities/ActivityForm/parse-activity.ts
+++ client/src/components/activities/ActivityForm/parse-activity.ts
@@ -14,43 +14,25 @@ import type { ActivityState } from "./activity-state.types";
  * because in this case the UI contains a date-input which only takes a date,
  * but for elsewhere in the UI, we need the timestamp to actually correspond to
  * the end of the day.
- *  */
+ */
 export function maybeShiftEndDateToEndOfDay(activity: ActivityState) {
-	if (activityGuards.withDates(activity)) {
-		activity.end_date = createDate(activity.end_date).endOf("day");
-	}
-}
+	const activityClone = { ...activity };
 
-/** Validation checks that to-post and to-put activities have in common go in here. */
-export function maybeThrowOnInvalidActivity(activity: ActivityState) {
-	if (!activityGuards.withDates(activity) && !activityGuards.withTimestamps(activity)) {
-		throw new Error("Activity must have either date fields or timestamp fields");
+	if (activityGuards.withDates(activityClone)) {
+		activityClone.end_date = createDate(activityClone.end_date).endOf("day");
 	}
+
+	return activityClone;
 }
 
 /** Parse the activity built in ActivityForm to the correct shape so we can make
  * a PUT request to update the activity. */
 export function parseUpdatedActivity(activity: Partial<ActivityWithIds>) {
-	if (!activity.activity_id) throw new Error("Activity must have a valid activity id");
-
-	maybeThrowOnInvalidActivity(activity);
-	maybeShiftEndDateToEndOfDay(activity);
-
-	return activityWithIdsSchema.parse(activity);
+	return activityWithIdsSchema.parse(maybeShiftEndDateToEndOfDay(activity));
 }
 
 /** Parses an incoming newActivity (from the ActivityForm component) into
  * something we can actually put in a request body. */
 export function parseNewActivity(newActivity: NewActivityInput) {
-	const requiredFields: (keyof NewActivityInput)[] = ["name"];
-	for (const field of requiredFields) {
-		if (newActivity[field] === undefined) {
-			throw new Error(`New activity is missing required field: ${field}`);
-		}
-	}
-
-	maybeThrowOnInvalidActivity(newActivity);
-	maybeShiftEndDateToEndOfDay(newActivity);
-
-	return newActivityInputSchema.parse(newActivity);
+	return newActivityInputSchema.parse(maybeShiftEndDateToEndOfDay(newActivity));
 }
