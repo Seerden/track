3c082c2ce74404fdd10a93262ba0761cdfd6bda7
Christiaan Seerden <christiaanseerden@live.nl>
2025-08-03 16:48:47 +0200

    remove unnecessary variable

--- server/src/lib/trpc/resolvers/activity/query-tasks.ts
+++ server/src/lib/trpc/resolvers/activity/query-tasks.ts
@@ -2,7 +2,5 @@ import { queryOverdueTasksByUser } from "@/lib/data/models/activities/query-task
 import { authenticatedProcedure } from "../../procedures/authenticated.procedure";
 
 export const queryOverdueTasks = authenticatedProcedure.query(async ({ ctx }) => {
-	const user_id = ctx.req.session.user.user_id;
-
-	return await queryOverdueTasksByUser({ user_id });
+	return await queryOverdueTasksByUser({ user_id: ctx.req.session.user.user_id });
 });
