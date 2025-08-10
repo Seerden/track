bb9af6bb6f3f23c5f6adea9f95d10b67aed64d1c
Christiaan Seerden <christiaanseerden@live.nl>
2025-08-10 15:27:45 +0200

    add a modal id for overdue tasks

--- client/src/lib/modal-ids.ts
+++ client/src/lib/modal-ids.ts
@@ -22,7 +22,10 @@ const modalIds = {
 	},
 	activities: {
 		form: "activity-form",
-		newTask: "new-task"
+		newTask: "new-task",
+		tasks: {
+			overdue: "overdue-tasks"
+		}
 	},
 	detailedActivity: "detailed-activity", // TODO: should be under activities.detailed like with habits and others
 	tagTree: { tree: "tag-tree", tagSelector: "tag-tree-tag-selector" },
