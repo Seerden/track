9c77f4d9a4ae021af8b85fefc9e671776b3d590a
Christiaan Seerden <christiaanseerden@live.nl>
2025-08-13 19:17:54 +0200

    clean up loader

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,16 +1,6 @@
 import Today from "@/components/Today/Today";
-import { queryClient } from "@/lib/query-client";
-import { trpc } from "@/lib/trpc";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-	component: Today,
-	// TODO TRK-246: remove this once the overdue task functionality is implemented
-	loader: async () => {
-		const overdue = await queryClient.ensureQueryData(
-			trpc.activities.tasks.overdue.queryOptions()
-		);
-
-		console.log({ overdue });
-	}
+	component: Today
 });
