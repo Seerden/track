33dd6f248c2482ea668400f6b5366c79d13e48a0
Christiaan Seerden <christiaanseerden@live.nl>
2025-08-15 14:15:00 +0200

    (re) inline props type; use Containers.Column for styled component

--- client/src/components/Today/Habits.tsx
+++ client/src/components/Today/Habits.tsx
@@ -2,24 +2,17 @@ import Empty from "@/components/Today/Empty";
 import Habit from "@/components/Today/Habit";
 import modalIds from "@/lib/modal-ids";
 import { useModalState } from "@/lib/state/modal-state";
-import { flex } from "@/lib/theme/snippets/flex";
-import { spacingValue } from "@/lib/theme/snippets/spacing";
-import styled from "@emotion/styled";
+import Containers from "@/lib/theme/components/container.style";
 import type { HabitWithPossiblySyntheticEntries } from "@shared/lib/schemas/habit";
 import type { ById } from "@shared/types/data/utility.types";
 import S from "./style/Habits.style";
 import T from "./style/Today.style";
 
-type HabitsProps = {
+export default function Habits({
+	habitsById
+}: {
 	habitsById: ById<HabitWithPossiblySyntheticEntries>;
-};
-
-const HabitWrapper = styled.div`
-	${flex.column};
-	gap: ${spacingValue.medium};
-`;
-
-export default function Habits({ habitsById }: HabitsProps) {
+}) {
 	const habits = Object.values(habitsById);
 	const { openModal } = useModalState();
 
@@ -28,11 +21,11 @@ export default function Habits({ habitsById }: HabitsProps) {
 			<T.BlockTitle>Habits</T.BlockTitle>
 
 			{habits.length > 0 ? (
-				<HabitWrapper>
+				<Containers.Column gap="medium">
 					{habits.map((habit) => (
 						<Habit key={habit.habit_id} habit={habit} />
 					))}
-				</HabitWrapper>
+				</Containers.Column>
 			) : (
 				<Empty action={() => openModal(modalIds.habits.new)}>
 					<span>No habits found for today. </span>
