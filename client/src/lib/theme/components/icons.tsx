167b56777fe6d8429334c85f019f987c106bcfa3
Christiaan Seerden <christiaanseerden@live.nl>
2025-08-17 20:47:39 +0200

    (style) use theme values in Shortcut icon

--- client/src/lib/theme/components/icons.tsx
+++ client/src/lib/theme/components/icons.tsx
@@ -1,26 +1,27 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import type { CSSProperties } from "react";
+import { font } from "../font";
 import { outline } from "../snippets/edge";
 import { flex } from "../snippets/flex";
 import { radius } from "../snippets/radius";
+import { spacing } from "../snippets/spacing";
 
 export const Icon = styled.span`
 	${flex.centered};
 	border-radius: 50%;
 `;
 
-// TODO (TRK-75): theme values
 const Shortcut = styled.span`
-	font-size: 0.8rem;
+	font-size: ${font.size["0.82"]};
 	color: #333;
 	font-weight: 500;
 	text-transform: uppercase;
-	padding: 0.2rem 0.5rem;
+	${spacing.padding.wide({ size: 0.2, ratio: 2.5 })};
 	background-color: #ddd;
 	width: max-content;
 	display: inline-flex;
-	border-radius: 3px;
+	${radius.small};
 	box-shadow: 0 0.2rem 0 -0.1rem #eee;
 `;
 
