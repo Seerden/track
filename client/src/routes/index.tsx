ccda8d82ad92c883d93c9fcd5b11e7d845edce0d
Christiaan Seerden <christiaanseerden@live.nl>
2025-08-22 12:45:02 +0200

    move pendingComponent from index.tsx to defaultPendingComponent on the router

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,21 +1,6 @@
-import { DefaultSkeleton } from "@/components/layout/Skeleton";
 import Today from "@/components/Today/Today";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-	component: Today,
-	pendingComponent: () => {
-		return (
-			<>
-				<div
-					style={{
-						padding: "3rem",
-						width: "100%"
-					}}
-				>
-					<DefaultSkeleton />
-				</div>
-			</>
-		);
-	}
+	component: Today
 });
