00adc5b6979d2d1582e8c91e37e1431027cd7f81
Christiaan Seerden <christiaanseerden@live.nl>
2025-09-13 10:33:50 +0200

    (types) extract deleteActivityById input to schema/type; check ownership before deleting an activity

--- server/src/lib/data/models/activities/delete-activity.ts
+++ server/src/lib/data/models/activities/delete-activity.ts
@@ -1,13 +1,30 @@
 import type { Activity } from "@shared/lib/schemas/activity";
+import { z } from "@shared/lib/zod";
 import type { Maybe } from "@trpc/server/unstable-core-do-not-import";
 import type { QueryFunction } from "types/sql.types";
 import { TABLES } from "types/tables";
 import { sqlConnection } from "@/db/init";
+import { queryActivityById } from "./query-activities";
+
+export const deleteActivityByIdInputSchema = z.object({
+	activity_id: z.string(),
+	/** used to verify ownership of the activity */
+	user_id: z.string(),
+});
+export type DeleteActivityByIdInput = z.infer<
+	typeof deleteActivityByIdInputSchema
+>;
 
 export const deleteActivityById: QueryFunction<
-	{ activity_id: string },
+	DeleteActivityByIdInput,
 	Promise<Maybe<Activity>>
-> = async ({ sql = sqlConnection, activity_id }) => {
+> = async ({ sql = sqlConnection, activity_id, user_id }) => {
+	const activity = await queryActivityById({ sql, activity_id, user_id });
+	if (!activity) {
+		// activity either did not exist, or user does not own it
+		return null;
+	}
+
 	const [deletedActivity] = await sql<[Activity?]>`
       delete from ${sql(TABLES.activities)}
       where activity_id = ${activity_id}
--- server/src/lib/trpc/resolvers/activity/delete-activity.ts
+++ server/src/lib/trpc/resolvers/activity/delete-activity.ts
@@ -1,17 +1,15 @@
-import { z } from "@shared/lib/zod";
-import { deleteActivityById } from "@/lib/data/models/activities/delete-activity";
+import {
+	deleteActivityById,
+	deleteActivityByIdInputSchema,
+} from "@/lib/data/models/activities/delete-activity";
 import { authenticatedProcedure } from "../../procedures/authenticated.procedure";
 
-// TODO: create a schema+type for the input, and use it in the inner function as well
 export const resolveDeleteActivityById = authenticatedProcedure
-	.input(
-		z.object({
-			activity_id: z.string(),
-		})
-	)
+	.input(deleteActivityByIdInputSchema)
 	.mutation(async ({ input, ctx }) => {
 		// TODO: check if activity belongs to the user before deleting
 		return await deleteActivityById({
 			activity_id: input.activity_id,
+			user_id: ctx.req.session.user.user_id,
 		});
 	});
