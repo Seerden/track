47bcab0f35d1d30b4ec125291296d052996edff6
Christiaan Seerden <christiaanseerden@live.nl>
2025-09-13 10:40:30 +0200

    (todo) resolved

--- server/src/lib/trpc/resolvers/activity/delete-activity.ts
+++ server/src/lib/trpc/resolvers/activity/delete-activity.ts
@@ -7,7 +7,6 @@ import { authenticatedProcedure } from "../../procedures/authenticated.procedure
 export const resolveDeleteActivityById = authenticatedProcedure
 	.input(deleteActivityByIdInputSchema)
 	.mutation(async ({ input, ctx }) => {
-		// TODO: check if activity belongs to the user before deleting
 		return await deleteActivityById({
 			activity_id: input.activity_id,
 			user_id: ctx.req.session.user.user_id,
