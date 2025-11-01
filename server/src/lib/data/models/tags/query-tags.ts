a25c2a9e10921b944767c2b3c71290d5b6675569
Christiaan Seerden <christiaanseerden@live.nl>
2025-11-01 20:33:29 +0100

    (feat) queryTagsForRecurringActivities

--- server/src/lib/data/models/tags/query-tags.ts
+++ server/src/lib/data/models/tags/query-tags.ts
@@ -1,3 +1,4 @@
+import type { Activity } from "@shared/lib/schemas/activity";
 import type { TagWithId } from "@shared/lib/schemas/tag";
 import type {
 	ActivityTagRelation,
@@ -5,6 +6,7 @@ import type {
 } from "@shared/types/data/relational.types";
 import type { ID } from "@shared/types/data/utility.types";
 import type { QueryFunction } from "types/sql.types";
+import { TABLES } from "types/tables";
 import { sqlConnection } from "@/db/init";
 
 /** Get all of a user's tags. */
@@ -57,6 +59,44 @@ export const queryTagRelations: QueryFunction<
 	return sql<[TagTagRelation]>`
       select * from tags_tags where user_id = ${user_id}
    `;
+};
+
+/** Given a list of recurrence ids, get all tag_ids (in a map, by the recurrence
+ * id of the original activity) of that belong to the activities of
+ * recurrence_id.
+ * @note this presumes that a recurring activity will never have tags that
+ * differ from its original activity. I am not sure if, when going from
+ * synthetic -> real, we do not "lock in" the tag_ids for the recurring entry. */
+export const queryTagsForRecurringActivities: QueryFunction<
+	{
+		user_id: ID;
+		recurrence_ids: ID[];
+	},
+	Promise<Map<string, string[]>>
+> = async ({ sql = sqlConnection, user_id, recurrence_ids }) => {
+	const activityTags = await sql<
+		(ActivityTagRelation & Pick<Activity, "recurrence_id">)[]
+	>`
+      select t.*, a.recurrence_id
+      from ${sql(TABLES.activitiesTags)} t
+      join ${sql(TABLES.activities)} a 
+         on a.activity_id = t.activity_id
+      where t.user_id = ${user_id} and
+         a.recurrence_id = any(${recurrence_ids}) and
+         a.will_recur = true
+   `;
+
+	// TODO: I thought this could be done with mapById, but that does something
+	// else, actually. I guess I need another function for this functionality,
+	// but what would I call it, listToMapById? Confusing if compared to mapById
+	// and groupById.
+	return activityTags.reduce((acc, cur) => {
+		if (!cur.recurrence_id) return acc;
+
+		const tagIds = acc.get(cur.recurrence_id) ?? [];
+		acc.set(cur.recurrence_id, tagIds.concat(cur.tag_id));
+		return acc;
+	}, new Map<string, TagWithId["tag_id"][]>());
 };
 
 export const queryTagsAndRelations: QueryFunction<
