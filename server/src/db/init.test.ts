d70af6d9aaf10eddc320a2fb7dc52431e8971702
Christiaan Seerden <christiaanseerden@live.nl>
2025-11-02 14:42:32 +0100

    testOptions: localhost when in test environment for locally run tests; rework db init test implementation

--- server/src/db/init.test.ts
+++ server/src/db/init.test.ts
@@ -1,8 +1,7 @@
-import { sqlConnection as sql } from "./init";
+import { pingDatabase } from "./init";
 
 describe("test-database connection", () => {
 	it("initializes", async () => {
-		const [result] = await sql`select array[1]`;
-		expect(result.array).toEqual([1]);
+		expect(async () => await pingDatabase()).not.toThrow();
 	});
 });
