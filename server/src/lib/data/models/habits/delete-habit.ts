10cd8ab8bfece4ff06e83f758ed204c98954e363
Christiaan Seerden <christiaanseerden@live.nl>
2025-11-04 12:06:41 +0100

    deleteHabitById: filter by user_id and rename resolver

--- server/src/lib/data/models/habits/delete-habit.ts
+++ server/src/lib/data/models/habits/delete-habit.ts
@@ -4,12 +4,13 @@ import type { QueryFunction } from "types/sql.types";
 import { sqlConnection } from "@/db/init";
 
 export const deleteHabitById: QueryFunction<
-	{ habit_id: ID },
+	{ habit_id: ID; user_id: ID },
 	Promise<{ habit_id: ID }[]>
-> = async ({ sql = sqlConnection, habit_id }) => {
-	return sql<[{ habit_id: Habit["habit_id"] }]>`
+> = async ({ sql = sqlConnection, habit_id, user_id }) => {
+	return sql<Pick<Habit, "habit_id">[]>`
       delete from habits
-      where habit_id = ${habit_id}
+         where habit_id = ${habit_id}
+         and user_id = ${user_id}
       returning habit_id
    `;
 };
--- server/src/lib/trpc/resolvers/habit/delete-habits.ts
+++ server/src/lib/trpc/resolvers/habit/delete-habits.ts
@@ -1,9 +1,10 @@
 import { z } from "@shared/lib/zod";
-import { deleteHabitById as _deleteHabitById } from "@/lib/data/models/habits/delete-habit";
+import { deleteHabitById } from "@/lib/data/models/habits/delete-habit";
 import { authenticatedProcedure } from "@/lib/trpc/procedures/authenticated.procedure";
 
-export const deleteHabitById = authenticatedProcedure
+export const deleteHabitByIdResolver = authenticatedProcedure
 	.input(z.string())
-	.mutation(async ({ input }) => {
-		return await _deleteHabitById({ habit_id: input });
+	.mutation(async ({ input, ctx }) => {
+		const user_id = ctx.req.session.user.user_id;
+		return await deleteHabitById({ habit_id: input, user_id });
 	});
