3566b63dc85f1f99bdbe4d486f7a88decda68778
Christiaan Seerden <christiaanseerden@live.nl>
2025-11-04 12:03:53 +0100

    add Sentry logging integration

--- server/instrument.ts
+++ server/instrument.ts
@@ -3,12 +3,19 @@ import * as Sentry from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import type { ErrorRequestHandler, Response } from "express";
 
-Sentry.init({
-	dsn: "https://f03ede6b86784fda49e2fc75bf8bb29e@o4508463516680192.ingest.de.sentry.io/4508574988828752",
-	integrations: [nodeProfilingIntegration()],
-	// Tracing
-	tracesSampleRate: 1.0, //  Capture 100% of the transactions
-});
+if (!Sentry.isInitialized()) {
+	Sentry.init({
+		environment: process.env.NODE_ENV,
+		enableLogs: true,
+		dsn: "https://f03ede6b86784fda49e2fc75bf8bb29e@o4508463516680192.ingest.de.sentry.io/4508574988828752",
+		integrations: [
+			nodeProfilingIntegration(),
+			Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+		],
+		tracesSampleRate: 1.0,
+		sendDefaultPii: true,
+	});
+}
 
 export { Sentry };
 
