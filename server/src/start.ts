8639ed273a61bb355318c401e1f1e64086c1dc22
Christiaan Seerden <christiaanseerden@live.nl>
2025-11-11 14:46:34 +0100

    (dev) do not DEV_seedScripts() on startup anymore

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,13 +1,10 @@
 import type { SeverityLevel } from "@sentry/core";
 import * as Sentry from "@sentry/node";
-import databaseScriptCache, {
-	DEV_seedScripts,
-} from "@/db/cache-script-executions";
+import databaseScriptCache from "@/db/cache-script-executions";
 import { sendTestNotification } from "./lib/notifications/test-notification";
 
 export async function runAtStartup() {
 	try {
-		await DEV_seedScripts();
 		await databaseScriptCache.synchronize();
 
 		console.log(await sendTestNotification());
