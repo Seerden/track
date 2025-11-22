47251b27e127f3417a2d56142048e3b0ec13d12c
Christiaan Seerden <christiaanseerden@live.nl>
2025-11-22 12:08:22 +0100

    Today: filter habits by tags

--- client/src/components/tags/TagFilter/filter-tags.ts
+++ client/src/components/tags/TagFilter/filter-tags.ts
@@ -1,22 +1,23 @@
 import type { PossiblySyntheticActivity } from "@shared/lib/schemas/activity";
+import type { HabitWithPossiblySyntheticEntries } from "@shared/lib/schemas/habit";
 import type { ID, Nullable } from "@shared/types/data/utility.types";
 import type { TagFilter } from "@/components/activities/ActivityFilter/tag-filter.atom";
 
 const tagPredicates = {
 	includes: (
-		activity: PossiblySyntheticActivity,
+		thing: PossiblySyntheticActivity | HabitWithPossiblySyntheticEntries,
 		tag_ids: Nullable<ID[]>,
 		exact?: boolean
 	) => {
 		if (!tag_ids?.length) return true;
 
 		return exact
-			? tag_ids.every((tag_id) => activity.tag_ids.includes(tag_id))
-			: tag_ids.some((tag_id) => activity.tag_ids.includes(tag_id));
+			? tag_ids.every((tag_id) => thing.tag_ids.includes(tag_id))
+			: tag_ids.some((tag_id) => thing.tag_ids.includes(tag_id));
 	},
 
 	excludes: (
-		activity: PossiblySyntheticActivity,
+		thing: PossiblySyntheticActivity | HabitWithPossiblySyntheticEntries,
 		tag_ids: Nullable<ID[]>,
 		exact?: boolean
 	) => {
@@ -24,17 +25,14 @@ const tagPredicates = {
 
 		// TODO: the non-exact case does not work as intended.
 		return exact
-			? !tag_ids.every((tag_id) => activity.tag_ids.includes(tag_id))
-			: !tag_ids.some((tag_id) => !activity.tag_ids.includes(tag_id));
+			? !tag_ids.every((tag_id) => thing.tag_ids.includes(tag_id))
+			: !tag_ids.some((tag_id) => !thing.tag_ids.includes(tag_id));
 	},
 };
 
-export function filterByTags(
-	activities: PossiblySyntheticActivity[],
-	filter: TagFilter
-) {
+export function filterByTags<
+	T extends PossiblySyntheticActivity | HabitWithPossiblySyntheticEntries,
+>(things: T[], filter: TagFilter) {
 	const predicate = tagPredicates[filter.type];
-	return activities.filter((activity) =>
-		predicate(activity, filter.value, filter.exact)
-	);
+	return things.filter((thing) => predicate(thing, filter.value, filter.exact));
 }
--- client/src/lib/hooks/useHabitsData.ts
+++ client/src/lib/hooks/useHabitsData.ts
@@ -1,13 +1,19 @@
+import { byIdAsList, mapById } from "@shared/lib/map";
+import type { Nullable } from "@shared/types/data/utility.types";
 import { useQuery } from "@tanstack/react-query";
 import { useAtomValue } from "jotai";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
+import { tagFilterAtom } from "@/components/activities/ActivityFilter/tag-filter.atom";
 import { withSyntheticHabitEntries } from "@/components/habits/Habits/synthetic";
+import { filterByTags } from "@/components/tags/TagFilter/filter-tags";
 import { trpc } from "@/lib/trpc";
 import type { TimeWindow } from "@/types/time-window.types";
 import { timeWindowAtom } from "../state/time-window.state";
 
 export default function useHabitsData() {
+	const tagFilter = useAtomValue(tagFilterAtom);
 	const { data: habitsData } = useQuery(trpc.habits.all.queryOptions());
+
 	const timeWindow = useAtomValue(timeWindowAtom);
 
 	const getHabitsForTimeWindow = useCallback(
@@ -26,12 +32,16 @@ export default function useHabitsData() {
 
 	const habits = getHabitsForTimeWindow(timeWindow);
 
+	const filteredHabits: Nullable<typeof habits> = useMemo(() => {
+		return mapById(filterByTags(byIdAsList(habits), tagFilter), "habit_id");
+	}, [habits, tagFilter]);
+
 	function getHabitById(id: string) {
-		return habitsData?.get(id);
+		return habits?.get(id);
 	}
 
 	return {
-		habits,
+		habits: filteredHabits,
 		/** The only place where this hook is used, doesn't use this. But can't
 		 * hurt to include it in the output anyway. */
 		habitsData,
