7513529a317cb146cbba48856439101537508a6f
Christiaan Seerden <christiaanseerden@live.nl>
2025-12-08 23:43:00 +0100

    (chore) remove wip index.tsx loader

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -3,11 +3,6 @@ import Today from "@/components/Today/Today";
 
 export const Route = createFileRoute("/")({
 	component: Home,
-	loader: async ({ context }) => {
-		await context.queryClient.ensureQueryData(
-			context.trpc.activities.all.queryOptions()
-		);
-	},
 });
 
 function Home() {
