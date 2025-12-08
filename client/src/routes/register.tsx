14b69d3e1204dd7d5dcaed8c23e7c36dd8f870f8
Christiaan Seerden <christiaanseerden@live.nl>
2025-12-08 21:35:50 +0100

    (chore) remove useRegister; rename *RegisterBetterAuth -> *Register

--- client/src/routes/register.tsx
+++ client/src/routes/register.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
-import RegisterBetterAuth from "@/components/auth/Register/RegisterBetterAuth";
+import Register from "@/components/auth/Register/Register";
 
 export const Route = createFileRoute("/register")({
-	component: RegisterBetterAuth,
+	component: Register,
 });
