86f99404097717e15aaeac230a9d815d8fb1e573
Christiaan Seerden <christiaanseerden@live.nl>
2025-12-10 13:15:39 +0100

    RequestPasswordReset: extract functionality to hook

--- client/src/components/auth/RequestPasswordReset.tsx
+++ client/src/components/auth/RequestPasswordReset.tsx
@@ -1,62 +1,10 @@
-import { notifications } from "@mantine/notifications";
-import { z } from "@shared/lib/zod";
-import { useMutation } from "@tanstack/react-query";
-import { LucideAsterisk, LucideSendHorizontal } from "lucide-react";
-import { type FormEvent, useState } from "react";
-import { authClient } from "@/lib/auth-client";
-import useAuthentication from "@/lib/hooks/useAuthentication";
-import { colors } from "@/lib/theme/colors";
+import { LucideSendHorizontal } from "lucide-react";
+import { useRequestPasswordReset } from "@/components/auth/useRequestPasswordReset";
 import F from "@/lib/theme/components/form/form.alternate.style";
 import S from "./style/auth.style";
 
-function useRequestResetMutation() {
-	return useMutation({
-		mutationFn: async (email: string) => {
-			return await authClient.requestPasswordReset({
-				email,
-			});
-		},
-	});
-}
-
 export default function RequestPasswordReset() {
-	const { currentUser } = useAuthentication();
-
-	const [email, setEmail] = useState(currentUser?.email ?? "");
-	const { mutate } = useRequestResetMutation();
-
-	function handleSubmit(e: FormEvent<HTMLFormElement>) {
-		e.preventDefault();
-		if (z.email().parse(email)) {
-			mutate(email, {
-				onError: (error) => {
-					notifications.show({
-						position: "top-center",
-						color: colors.red.main,
-						message: error.message ?? error.name,
-					});
-				},
-				onSuccess: ({ error, data }) => {
-					if (error) {
-						notifications.show({
-							position: "top-center",
-							color: "orangered",
-							message: error.message ?? error.code,
-						});
-					} else {
-						if (data.message) {
-							notifications.show({
-								position: "top-center",
-								icon: <LucideAsterisk />,
-								color: "royalblue",
-								message: data.message,
-							});
-						}
-					}
-				},
-			});
-		}
-	}
+	const { email, handleSubmit, setEmail } = useRequestPasswordReset();
 
 	return (
 		<S.Wrapper>
