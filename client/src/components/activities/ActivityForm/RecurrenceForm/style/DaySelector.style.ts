844355221b4cc6cf2b85dc19d8c22eb3e1d0ebc8
Christiaan Seerden <christiaanseerden@live.nl>
2025-12-18 10:55:15 +0100

    (style) DaySelector: add dark-mode styles

--- client/src/components/activities/ActivityForm/RecurrenceForm/style/DaySelector.style.ts
+++ client/src/components/activities/ActivityForm/RecurrenceForm/style/DaySelector.style.ts
@@ -4,7 +4,6 @@ import formStyle from "@/lib/theme/components/form.style";
 import Input from "@/lib/theme/input";
 import Active from "@/lib/theme/snippets/active";
 import { noBorders } from "@/lib/theme/snippets/border";
-import { outline } from "@/lib/theme/snippets/edge";
 import { radius } from "@/lib/theme/snippets/radius";
 import { spacing, spacingValue } from "@/lib/theme/snippets/spacing";
 
@@ -30,14 +29,13 @@ const Trigger = styled(Buttons.Unstyled)`
 
 // TODO: put this in floating.style.ts!
 const FloatingWrapper = styled.div`
-	${spacing.padding.small};
-	${outline.grey};
-	${radius.medium};
-
-	background-color: ${(p) => p.theme.colors.background.main[3]};
-	box-shadow: 0 0.2rem 0.5rem -0.1rem #aaa;
-
 	z-index: 10;
+	${spacing.padding.small};
+	${radius.medium};
+	outline: 2px solid var(--bg-5-2);
+	background-color: ${(p) => p.theme.colors.background.main[3]};
+	box-shadow: 0 0.2rem 0.5rem -0.1rem var(--bg-5-1);
+
 `;
 
 // TODO: put this in input.style
