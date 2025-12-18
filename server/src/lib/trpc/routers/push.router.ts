19ed099c9663f97f38a7c69c6f82fcbe83a83db2
Christiaan Seerden <christiaanseerden@live.nl>
2025-12-18 14:10:42 +0100

    remove ".resolver" from resolver filenames

--- server/src/lib/trpc/routers/push.router.ts
+++ server/src/lib/trpc/routers/push.router.ts
@@ -1,6 +1,6 @@
-import { pushSubscriptionQuery } from "@/lib/trpc/resolvers/push/query-push-subscription.resolver";
-import { subscribeMutation } from "../resolvers/push/subscribe.resolver";
-import { testNotificationQuery } from "../resolvers/push/test-notification.resolver";
+import { pushSubscriptionQuery } from "@/lib/trpc/resolvers/push/query-push-subscription";
+import { subscribeMutation } from "../resolvers/push/subscribe";
+import { testNotificationQuery } from "../resolvers/push/test-notification";
 import { t } from "../trpc-context";
 
 export const pushRouter = t.router({
--- server/src/lib/trpc/routers/users.router.ts
+++ server/src/lib/trpc/routers/users.router.ts
@@ -1,6 +1,6 @@
 import { meQuery } from "@/lib/trpc/resolvers/users/me";
-import { userSettingsQuery } from "@/lib/trpc/resolvers/users/settings/query-settings.resolver";
-import { updateUserSettingsMutation } from "@/lib/trpc/resolvers/users/settings/update-settings.resolver";
+import { userSettingsQuery } from "@/lib/trpc/resolvers/users/settings/query-settings";
+import { updateUserSettingsMutation } from "@/lib/trpc/resolvers/users/settings/update-settings";
 import { t } from "@/lib/trpc/trpc-context";
 
 export const usersRouter = t.router({
