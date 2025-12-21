c325a68b3c083d3db82d079d7db656136b697d93
Christiaan Seerden <christiaanseerden@live.nl>
2025-12-21 17:44:17 +0100

    close modal on habit delete; use useQueryClient

--- client/src/lib/hooks/query/habits/useMutateDeleteHabit.ts
+++ client/src/lib/hooks/query/habits/useMutateDeleteHabit.ts
@@ -1,8 +1,12 @@
-import { useMutation } from "@tanstack/react-query";
-import { queryClient } from "@/lib/query-client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import modalIds from "@/lib/modal-ids";
+import { useModalState } from "@/lib/state/modal-state";
 import { trpc } from "@/lib/trpc";
 
 export default function useMutateDeleteHabit() {
+	const queryClient = useQueryClient();
+	const { closeModal } = useModalState();
+
 	return useMutation(
 		trpc.habits.m.delete.mutationOptions({
 			onSuccess: () => {
@@ -12,6 +16,7 @@ export default function useMutateDeleteHabit() {
 				queryClient.invalidateQueries({
 					queryKey: trpc.habits.q.entries.queryKey(),
 				});
+				closeModal(modalIds.habits.detailed);
 			},
 		})
 	);
