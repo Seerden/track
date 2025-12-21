c54c3f527fad10c95855d58a5e81a34bbd36c5ab
Christiaan Seerden <christiaanseerden@live.nl>
2025-12-21 15:22:40 +0100

    HabitForm: remove duplicate container, use context hook instead of inner useNewHabit hook

--- client/src/components/habits/HabitForm/HabitForm.tsx
+++ client/src/components/habits/HabitForm/HabitForm.tsx
@@ -3,15 +3,17 @@ import DateFields from "@/components/habits/HabitForm/DateField";
 import OccurrenceFields from "@/components/habits/HabitForm/OccurrenceFields";
 import ProgressionFields from "@/components/habits/HabitForm/ProgressionFields";
 import SimpleField from "@/components/habits/HabitForm/SimpleField";
-import { HabitFormProvider } from "@/components/habits/HabitForm/useHabitFormContext";
+import {
+	HabitFormProvider,
+	useHabitFormContext,
+} from "@/components/habits/HabitForm/useHabitFormContext";
 import { TAG_SELECTOR_IDS } from "@/components/tags/TagSelector/constants";
 import TagSelector from "@/components/tags/TagSelector/TagSelector";
 import modalIds from "@/lib/modal-ids";
 import Buttons from "@/lib/theme/components/buttons";
-import useNewHabit from "./useNewHabit";
 
 export default function HabitForm() {
-	const { onSubmit } = useNewHabit();
+	const { onSubmit } = useHabitFormContext();
 
 	return (
 		<HabitFormProvider>
@@ -25,15 +27,7 @@ export default function HabitForm() {
 
 					<OccurrenceFields />
 					<ProgressionFields />
-
-					<Form.Row
-						style={{
-							position: "relative",
-							flexDirection: "column",
-						}}
-					>
-						<DateFields />
-					</Form.Row>
+					<DateFields />
 
 					<TagSelector
 						tagSelectorId={TAG_SELECTOR_IDS.DEFAULT}
