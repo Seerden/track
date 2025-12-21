edb53345b7b15ee9dbb5794e84049963908bc059
Christiaan Seerden <christiaanseerden@live.nl>
2025-12-21 17:41:39 +0100

    add a zod schema and type for habitUpdateInput

--- shared/src/lib/schemas/habit.input.ts
+++ shared/src/lib/schemas/habit.input.ts
@@ -1,14 +1,21 @@
-import { newHabitSchema } from "@shared/lib/schemas/habit";
+import { habitSchema, newHabitSchema } from "@shared/lib/schemas/habit";
 import { z } from "@shared/lib/zod";
 
 export const habitInputSchema = z.object({
 	habit: newHabitSchema,
 	tagIds: z.array(z.string()).optional(),
 });
-export type HabitInput = z.infer<typeof habitInputSchema>;
 
 export const habitEntryUpdateInputSchema = z.object({
 	habit_entry_id: z.string(),
 	value: z.string(), // TODO: type for Varchar
 });
+
+export const habitUpdateInputSchema = z.object({
+	habit: habitSchema,
+	tagIds: z.array(z.string()).optional(),
+});
+
+export type HabitInput = z.infer<typeof habitInputSchema>;
 export type HabitEntryUpdateInput = z.infer<typeof habitEntryUpdateInputSchema>;
+export type HabitUpdateInput = z.infer<typeof habitUpdateInputSchema>;
