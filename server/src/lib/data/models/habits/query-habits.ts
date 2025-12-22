b4901b058fd2217a802adc10233ef45756a3852c
Christiaan Seerden <christiaanseerden@live.nl>
2025-12-22 01:39:38 +0100

    (chore) TODO regarding ownership checking

--- server/src/lib/data/models/habits/query-habits.ts
+++ server/src/lib/data/models/habits/query-habits.ts
@@ -7,7 +7,8 @@ import { query } from "@/lib/query-function";
 /** Check (by id) if a user owns a habit.
  * @todo we could cache this, but we don't even cache habits yet, so let's start
  * with that. Update mutations don't occur that often, so small performance wins
- * on them aren't that important at this stage. */
+ * on them aren't that important at this stage.
+ * @note may be replaced by other functionality TRK-298. */
 export const userOwnsHabit = query(
 	async (sql, { user_id, habit_id }: { user_id: ID; habit_id: ID }) => {
 		const [response] = await sql<{ count: string }[]>`
