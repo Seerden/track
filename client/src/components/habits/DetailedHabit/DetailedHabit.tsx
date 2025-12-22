abe6e999f08a95bb7de461b3f8649a7a383ad4a5
Christiaan Seerden <christiaanseerden@live.nl>
2025-12-22 15:07:10 +0100

    move DetailedHabit functionality into a hook

--- client/src/components/habits/DetailedHabit/DetailedHabit.tsx
+++ client/src/components/habits/DetailedHabit/DetailedHabit.tsx
@@ -1,19 +1,13 @@
 import { isNullish } from "@shared/lib/is-nullish";
 import type { HabitWithEntries } from "@shared/lib/schemas/habit";
-import { useAtomValue } from "jotai";
 import { LucidePenLine } from "lucide-react";
-import type { MouseEvent, PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
+import { useDetailedHabit } from "@/components/habits/DetailedHabit/useDetailedHabit";
 import HabitForm from "@/components/habits/HabitForm/HabitForm";
 import Modal from "@/components/utility/Modal/Modal";
 import TwoStepDelete from "@/components/utility/Modal/TwoStepDelete";
-import { createDate } from "@/lib/datetime/make-date";
-import useMutateDeleteHabit from "@/lib/hooks/query/habits/useMutateDeleteHabit";
-import { useQueryTags } from "@/lib/hooks/query/tags/useQueryTags";
-import useDetailedItemModal from "@/lib/hooks/useDetailedItemModal";
-import useHabitsData from "@/lib/hooks/useHabitsData";
 import modalIds from "@/lib/modal-ids";
 import { useModalState } from "@/lib/state/modal-state";
-import { timeWindowAtom } from "@/lib/state/time-window.state";
 import Buttons from "@/lib/theme/components/buttons";
 import Card from "@/lib/theme/components/Card.style";
 import { ActionBar } from "@/lib/theme/components/containers/action-bar.style";
@@ -26,39 +20,19 @@ type DetailedHabitProps = {
 
 // TODO: extract functionality to a hook
 export default function DetailedHabit({
-	habit: _habit,
+	habit: existingHabit,
 }: PropsWithChildren<DetailedHabitProps>) {
-	const { data: tags } = useQueryTags();
-	// NOTE: we do not use getHabitsForTimeWindow, because for the habit
-	// calendar, we want to create synthetic habits for potentially any date.
-	const { getHabitById } = useHabitsData();
-	const habit = getHabitById(_habit.habit_id);
 	const { openModal } = useModalState();
-
-	const { openDetailedItemModal } = useDetailedItemModal(
-		"tag",
-		modalIds.tags.detailed
-	);
-	const timeWindow = useAtomValue(timeWindowAtom);
-
-	if (!habit) return null;
-
-	const humanizedStart = createDate(habit.start_timestamp).fromNow();
-	const humanizedEnd = habit.end_timestamp
-		? createDate(habit.end_timestamp).fromNow()
-		: null;
-	const humanizedFrequency = `${habit.frequency} time(s) per ${habit.interval} ${habit.interval_unit}(s)`; // TODO: consider using frequencyString from elsewhere
-
-	const { mutate } = useMutateDeleteHabit();
-
-	function handleDeleteHabit(e: MouseEvent<HTMLButtonElement>) {
-		e.stopPropagation();
-
-		// notification
-		if (!habit) return;
-
-		mutate(habit.habit_id);
-	}
+	const {
+		habit,
+		handleDeleteHabit,
+		humanizedFrequency,
+		humanizedStart,
+		humanizedEnd,
+		tags,
+		openDetailedItemModal,
+		timeWindow,
+	} = useDetailedHabit(existingHabit);
 
 	return (
 		<>
@@ -114,13 +88,7 @@ export default function DetailedHabit({
 				{tags && (
 					<Card.Tags>
 						{habit.tag_ids.map((id) => (
-							<Card.Tag
-								key={id}
-								onClick={(e) => {
-									e.stopPropagation();
-									openDetailedItemModal(id);
-								}}
-							>
+							<Card.Tag key={id} onClick={() => openDetailedItemModal(id)}>
 								{/* TODO: See #176 */}
 								{tags.get(id)?.name}
 							</Card.Tag>
