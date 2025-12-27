2982471e5d2cfe7425eab0e9f74742c7bae1796c
Christiaan Seerden <christiaanseerden@live.nl>
2025-12-27 12:45:06 +0100

    login route: reset queries beforeLoad

--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -3,6 +3,9 @@ import Login from "@/components/auth/Login/Login";
 
 export const Route = createFileRoute("/login")({
 	component: Login,
+	beforeLoad: async ({ context: { queryClient } }) => {
+		queryClient.resetQueries();
+	},
 	head: (_ctx) => ({
 		meta: [{ title: "Login" }],
 	}),
