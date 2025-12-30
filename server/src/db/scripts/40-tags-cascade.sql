1b708097442a473557474e2b4db39413a9245089
Christiaan Seerden <christiaanseerden@live.nl>
2025-12-30 13:02:44 +0100

    tags-cascade migration: drop constraint before creating it

--- server/src/db/scripts/40-tags-cascade.sql
+++ server/src/db/scripts/40-tags-cascade.sql
@@ -1,1 +1,3 @@
-alter table activities_tags add constraint fk_activities_tags_tags foreign key (tag_id) references tags (tag_id) on delete cascade;
+alter table activities_tags
+drop constraint fk_activities_tags_tags,
+add constraint fk_activities_tags_tags foreign key (tag_id) references tags (tag_id) on delete cascade;
